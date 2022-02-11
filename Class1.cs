e2282203e4c2346945062d44a22346ef73e613d0
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:19:29 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿//wap to accet no. from the user and print sq
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Class1
+    {
+        //public static void Main()
+        //{
+        //    int num, sq;
+        //    Console.WriteLine("Entrt Number");
+        //    num = Convert.ToInt32(Console.ReadLine());
+        //    sq = num * num;
+        //    Console.WriteLine("Square= "+sq);
+        //    Console.WriteLine();
+
+
+
+        //}
+         
+
+    }
+}
