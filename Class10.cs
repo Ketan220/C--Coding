caf8e1ea5067fe0fd10d35f4549f299de483dbf6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:26:29 +0530

    Add files via upload

---
+++ Class10.cs
@@ -0,0 +1,26 @@
+﻿//5 - float 
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class10
+    {
+        static void Main()
+        {
+            float b= -1234.5667890f;
+            Console.WriteLine(b);
+            Console.WriteLine("Entre value");
+            b=Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Float="+b);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+*/
