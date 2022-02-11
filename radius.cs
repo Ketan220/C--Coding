73d139eed2632da878c0de7568ff4a91a777e9f3
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:22:31 +0530

    Add files via upload

---
+++ radius.cs
@@ -0,0 +1,27 @@
+﻿//wap to accept radius and print area and circ of circle
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class radius
+    {
+        //static void Main()
+        //{
+        //    int radius;
+        //    float area, cir;
+        //    Console.WriteLine("Enter Radius");
+        //    radius = Convert.ToInt32(Console.ReadLine());
+        //    area = (float)3.14f * radius * radius;
+        //    cir = (float)2 * 3.14f* radius;
+        //    Console.WriteLine("Area of Circumference=" + area);
+        //    Console.WriteLine("circumference of circle=" + cir);
+        //    Console.WriteLine();
+
+
+        //}
+    }
+}
