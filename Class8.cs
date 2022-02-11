8a438bbbf285e4f3c896a1271a22e896ba12b120
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:26:01 +0530

    Add files via upload

---
+++ Class8.cs
@@ -0,0 +1,31 @@
+﻿//3 int and uint
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class8
+    {
+        static void Main()
+        {
+            int b5 = -12345;
+            Console.WriteLine(b5);
+            Console.WriteLine("Enter value");
+            b5=Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Int=" + b5);
+
+            int b6 = 123456789;
+            Console.WriteLine(b6);
+            Console.WriteLine("Enter value");
+            b6 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Int=" + b6);
+
+
+
+        }
+    }
+}
+*/
