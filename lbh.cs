5b5c595931c897c21c53d0773e3a02cd3901bf06
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:21:41 +0530

    Add files via upload

---
+++ lbh.cs
@@ -0,0 +1,26 @@
+﻿//6 - wap to accept len, bread, and height and print volume 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class lbh
+    {
+        //static void Main()
+        //{
+        //    int l, b, h;
+        //    float volume;
+        //    Console.WriteLine("Enter the length breadth height");
+        //    l = Convert.ToInt32(Console.ReadLine());
+        //    b = Convert.ToInt32(Console.ReadLine());
+        //    h = Convert.ToInt32(Console.ReadLine());
+        //    volume = (float)l * b * h;
+        //    Console.WriteLine("Volume=" + volume);
+        //    Console.WriteLine();
+
+        //}
+    }
+}
