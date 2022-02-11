30d41dbad54e0935a1c496a136844fdb12316d10
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:28:41 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿//See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿//program to print hello
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+    //   // static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("hello");
+    //        //console is a class
+    //        //writeline is a method
+    //        Console.ReadLine();
+    //    }
+    }
+}
