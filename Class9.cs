587813dedf2b1a81fad1dd916ca3724f427d17d7
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:26:13 +0530

    Add files via upload

---
+++ Class9.cs
@@ -0,0 +1,30 @@
+﻿//4 - long and ulong
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class9
+    {
+        static void Main()
+        {
+            long b7 = -83848858L;
+            Console.WriteLine(b7);
+            Console.WriteLine("Enter value");
+            b7=Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("lONG=" + b7);
+
+            ulong b8 = 83848858L;
+            Console.WriteLine(b7);
+            Console.WriteLine("Enter value");
+            b8 = Convert.ToUInt64(Console.ReadLine());
+            Console.WriteLine("lONG=" + b8);
+
+            Console.ReadLine();
+        }
+    }
+}
+*/
