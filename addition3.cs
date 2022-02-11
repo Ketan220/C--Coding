731cf18c1f872d4789943cbba6f4535320b97d8f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:21:19 +0530

    Add files via upload

---
+++ addition3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class addition3
+    {
+        //static void Main()
+        //{
+        //   int n1, n2,n3, res;
+        //   Console.WriteLine("Enter three number");
+        //   n1 = Convert.ToInt32(Console.ReadLine());
+        //   n2 = Convert.ToInt32(Console.ReadLine());
+        //   n3 = Convert.ToInt32(Console.ReadLine());
+        //   res = n1 + n2+n3;
+        //   Console.WriteLine("Addition=" + res);
+        //   Console.WriteLine();
+
+
+        //}
+    }
+}
