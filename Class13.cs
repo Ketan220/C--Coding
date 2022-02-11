e86bb9b681dfac99055f46273f48e4c533849263
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:27:15 +0530

    Add files via upload

---
+++ Class13.cs
@@ -0,0 +1,27 @@
+﻿//8 - char datatype
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class13
+    {
+        public static void Main()
+        {
+            char b = 'M';
+            Console.WriteLine(" value " + b);
+            Console.WriteLine("enter value ");
+            b = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("value = " + b);
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
+*/
