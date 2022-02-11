5642d89f21a45f73255b4b6ff4a75c2cb5ad2090
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:24:17 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class2
+    {
+        enum grade
+        {
+            first = 90, second = 34, third = 44
+        };
+        //static void Main()
+        //{
+        //    grade g;
+        //    g = (grade.third);
+        //    Console.WriteLine("Grade=" + g);
+        //    Console.WriteLine("Grade="+ Convert.ToInt32(g));
+        //    Console.WriteLine();
+        //}
+     
+
+    }
+}
