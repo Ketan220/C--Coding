fd62030901fe5dc28fb8bd37a0503d77757d76b1
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:24:34 +0530

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,35 @@
+﻿//3 - create  a structure student having members rno, name, course
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class3
+    {
+        struct student
+        {
+            public int rno;
+            public string name;
+            public string corse;
+        };
+        static void Main()
+        {
+            student s1;
+            s1.rno=32;
+            s1.name = "ketan";
+            s1.corse= "DOTNET";
+            Console.WriteLine(s1.rno);
+            Console.WriteLine(s1.name);
+            Console.WriteLine(s1.corse);
+            Console.WriteLine();
+
+
+
+
+        }
+    }
+}
+*/
