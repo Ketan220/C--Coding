c80c4cb5ff1a9087378256acf7d74c40107f4d91
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:26:44 +0530

    Add files via upload

---
+++ Class11.cs
@@ -0,0 +1,22 @@
+﻿//6 - double datatype
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class11
+    {
+        static void Main()
+        {
+            double b = -1234.57858758;
+            Console.WriteLine(b);
+            Console.WriteLine("Enter value");
+            b=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Value="+b);
+        }
+    }
+}
+*/
