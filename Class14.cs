b6f97b2a0d5e613cd105ef3fec61f956d0bd8c82
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:27:32 +0530

    Add files via upload

---
+++ Class14.cs
@@ -0,0 +1,35 @@
+﻿//if else
+//wap to accept 2 no and print greater no.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class14
+    {
+        public static void Main()
+        {
+            int n1, n2;
+            Console.WriteLine("Enter two numbers");
+            n1=Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            if (n1 > n2) 
+            {
+                Console.WriteLine("n1 is grater");
+            }
+            else
+            {
+                Console.WriteLine("n2 is greater");
+            }
+            Console.ReadLine();
+
+        
+        
+        }
+
+    }
+}
+*/
