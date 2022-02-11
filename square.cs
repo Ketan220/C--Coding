215515d06e3c0c2e3a52032b8116532e1ddf52ee
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:22:50 +0530

    Add files via upload

---
+++ square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class square
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("ketan");
+        //    console is a class
+        //    writeline is a method
+        //    Console.ReadLine();
+        //}
+
+}
+}
