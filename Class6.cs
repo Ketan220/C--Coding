1d8afddc9a2ea9e62ef879c0f5fa11577b975ff5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:25:27 +0530

    Add files via upload

---
+++ Class6.cs
@@ -0,0 +1,30 @@
+﻿//1- byte and sbyte
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class6
+    {
+        static void Main()
+        {
+            byte b = 25;// unsigned byte for storing positive no.
+            Console.WriteLine("Byte="+b);
+            Console.WriteLine("Enter byte");
+            b=Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Byte=" + b);
+
+            sbyte b1= -34;//for negative as well as positive no.
+            Console.WriteLine("Byte=" + b1);
+            Console.WriteLine("Enter number");
+            b1=Convert.ToSByte(Console.ReadLine());
+            Console.WriteLine("Sbyte=" + b1);
+            Console.WriteLine();
+
+        }
+    }
+}
+*/
