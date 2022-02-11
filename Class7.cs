144c42ac4883e157018d958a47f2e70a3cbd9221
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:25:43 +0530

    Add files via upload

---
+++ Class7.cs
@@ -0,0 +1,31 @@
+﻿//2 -short and ushort
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class7
+    {
+        static void Main()
+        {
+            short b2 = 12347;
+            Console.WriteLine(b2);
+            Console.WriteLine("Enter short value");
+            b2=Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Short="+b2);
+
+
+            ushort b3 = 12345;
+            Console.WriteLine(b3);
+            Console.WriteLine("Enter ushort value");
+            b3 = Convert.ToUInt16(Console.ReadLine());
+            Console.WriteLine("ushort=" + b3);
+
+
+        }
+    }
+}
+*/
