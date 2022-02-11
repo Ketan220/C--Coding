cbe51fae2d9d4ce714dbde4cbd407d2f8eb26d8a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:27:01 +0530

    Add files via upload

---
+++ Class12.cs
@@ -0,0 +1,27 @@
+﻿//7 boolean datatype
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class12
+    {
+        public static void Main()
+        {
+            Boolean b = true;
+            Console.WriteLine(" value " + b);
+            Console.WriteLine("enter value ");
+            b = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("value = " + b);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+*/
