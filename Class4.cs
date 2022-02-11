7aae77be06e6acd0484c388c1e8d7afb2e03cd18
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:24:55 +0530

    Add files via upload

---
+++ Class4.cs
@@ -0,0 +1,39 @@
+﻿//4 create a structure student and accept values from the user
+/*
+uing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class4
+    {
+        struct student
+        {
+            public int rno;
+            public string name;
+            public string course;
+        };
+        static void Main()
+        {
+            student s1;
+            Console.WriteLine("Enter rno");
+            s1.rno=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter name");
+            s1.name=Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter course name");
+            s1.course=Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Rollno="+s1.rno);
+            Console.WriteLine("name="+s1.name);
+            Console.WriteLine("Course="+s1.course);
+            Console.WriteLine();
+            
+
+
+        }
+    }
+}
+*/
