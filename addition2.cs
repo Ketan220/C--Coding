81e6befa3d33d86f641ebda8f3b652edf9f4ff9d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:20:52 +0530

    Add files via upload

---
+++ addition2.cs
@@ -0,0 +1,26 @@
+﻿////wap to accept 2 no. and display addtion of 2 no.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class addition2
+    {
+        //static void Main()
+        //{
+        //    int n1, n2, res;
+        //    Console.WriteLine("Enter first number");
+        //    n1 = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Enter first number");
+        //    n2 = Convert.ToInt32(Console.ReadLine());
+        //    res = n1 + n2;
+        //    Console.WriteLine("Addition=" + res);
+        //    Console.WriteLine();
+
+
+        //}
+    }
+}
