4860df868920ac565ad8b2a17bd12dd6596ffa50
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:27:48 +0530

    Add files via upload

---
+++ Class15.cs
@@ -0,0 +1,44 @@
+﻿//if else if
+////wap to accept 3 subject marks  and print total , per and grade.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class15
+    {
+        public static void Main()
+        {
+            int m1, m2, m3,total;
+            float per;
+            string grade;
+            Console.WriteLine("Entre three number");
+            m1=Convert.ToInt32(Console.ReadLine()); 
+            m2=Convert.ToInt32(Console.ReadLine()); 
+            m3=Convert.ToInt32(Console.ReadLine());
+            total = m1 + m2 + m3;
+            per = (float)(total /300.0f) *100.0f;
+            Console.WriteLine("Total="+total);
+            Console.WriteLine("Percentage="+per);
+            if(per>57)
+            {
+                grade = "Distinction";
+
+            }
+            else if(per>=60 && per<75)
+            {
+                grade = "First";
+            }
+            else
+            {
+                grade = "Second";
+            }
+            Console.WriteLine("Grade="+grade);
+            
+        }
+    }
+}
+*/
