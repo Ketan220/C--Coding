88810cb991b33abadef612c1b7dffeb33b78d26f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:21:59 +0530

    Add files via upload

---
+++ name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class name
+    {
+        static void Main()
+        {
+            string name;
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Name="+ name);
+            Console.WriteLine();
+        }
+    }
+}
