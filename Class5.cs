98c018b43db46f99b8e0de6ab20c545be6258b6d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-11 21:25:10 +0530

    Add files via upload

---
+++ Class5.cs
@@ -0,0 +1,20 @@
+﻿//Explicit conversion from int to long
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class5
+    {
+        static void Main()
+        {
+            int intvalue = 123;
+            long longValue = intvalue;
+            Console.WriteLine("(long) {0}={1}", longValue, intvalue);
+        }
+    }
+}
+*/
