1a8a8cff79c17a06fc51d775a9c7ded53ab9f1a8
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:57:16 +0530

    Add files via upload

---
+++ Class35.cs
@@ -0,0 +1,27 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class35
+    {
+        public static void Main()
+        {
+            int i, sum = 0, a, b;
+            Console.WriteLine("Enter two numbers");
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            i = a;
+            while(i<=b)
+            {
+                sum = sum + i;
+                i++;
+            }
+            Console.WriteLine("Summatio=" + sum);
+        }
+    }
+}
+*/
