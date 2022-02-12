76a4b40ca7e57757676c80c5dd79b31c5fab15fb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:56:27 +0530

    Add files via upload

---
+++ Class29.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class29
+    {
+       public static void Main()
+        {
+            char ch;
+            Console.WriteLine("Enter character");
+            ch = Convert.ToChar(Console.ReadLine());
+            if(ch>=97 && ch<=122)
+            {
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+                {
+                    Console.WriteLine("Alphabet is Vowel");
+                }
+                else
+                {
+                    Console.WriteLine("Alphabet is not Vowel");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Alphabet is not lower case");
+
+            }
+        }
+       
+    }
+}
+*/
