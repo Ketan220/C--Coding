1ebc68529901e42edf25c9707399b6ce55443e56
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:57:49 +0530

    Add files via upload

---
+++ Class37.cs
@@ -0,0 +1,48 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class37
+    {
+        public static void Main()
+        {
+            int n, m, factn = 1, factm = 1, factnm = 1, i = 1;
+            float c;
+            Console.WriteLine("Enter the value of n and m");
+            n=Convert.ToInt32(Console.ReadLine());
+            m=Convert.ToInt32(Console.ReadLine());
+            if(n>m)
+            {
+                while(i<=n)
+                {
+                    factn = factn * i;
+                    i++;
+                }
+                i = 1;
+                while(i<=m)
+                {
+                    factm = factm * i;
+                    i++;
+                }
+                i = 1;
+                while(i<=n-m)
+                {
+                    factnm = factnm * i;
+                    i++;
+                }
+                c = (factn * 0.1f) / (factm * factnm);
+                Console.WriteLine("C="+c);
+                
+            }
+            else
+            {
+                Console.WriteLine("Equation cant not be solved");
+            }
+
+        }
+    }
+}*/
