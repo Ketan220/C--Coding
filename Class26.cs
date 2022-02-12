0b747f9fcf78f6697ca8b2f9655580781613e3a6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:55:32 +0530

    Add files via upload

---
+++ Class26.cs
@@ -0,0 +1,27 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class26
+    {
+        public static void Main()
+        {
+            int year;
+            Console.WriteLine("Enter year");
+            year=Convert.ToInt32(Console.ReadLine());
+            if((year%100==0 && year%400==0) || (year%100!=0 && year%4==0))
+            {
+                Console.WriteLine("Year is leap");
+            }
+            else
+            {
+                Console.WriteLine("Year is not leap");
+            }
+        }
+    }
+}
+*/
