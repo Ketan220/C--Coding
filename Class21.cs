0c8ed738128e076da2ccab26d9d3d86ff897aae4
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:54:15 +0530

    Add files via upload

---
+++ Class21.cs
@@ -0,0 +1,29 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class21
+    {
+        public static void Main()
+        {
+            int num;
+            Console.WriteLine("Entre number");
+            num=Convert.ToInt32(Console.ReadLine());
+            if(num%2==0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+*/
