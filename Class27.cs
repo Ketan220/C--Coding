a99970c8eb6df69eae0680f94daac26d53b32714
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:55:49 +0530

    Add files via upload

---
+++ Class27.cs
@@ -0,0 +1,24 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class27
+    {
+        public static void Main()
+        {
+            int ch;
+            Console.WriteLine("Enter character");
+            ch=Convert.ToChar(Console.ReadLine());
+            if(ch>='a' && ch<='z')
+            {
+                ch = ch - 32;
+            }
+            Console.WriteLine("Uppercase=" + ch);
+
+        }
+    }
+}*/
