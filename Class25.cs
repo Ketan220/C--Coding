9b2e0f35af0acf504e4ffcc9ce5b2fae43720e9c
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:55:18 +0530

    Add files via upload

---
+++ Class25.cs
@@ -0,0 +1,49 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class25
+    {
+        public static void Main()
+        {
+            float x, y;
+            Console.WriteLine("Enter x and y");
+            x=Convert.ToSingle(Console.ReadLine());
+            y=Convert.ToSingle(Console.ReadLine());
+            if(x>0 && y>0)
+            {
+                Console.WriteLine("Points lies in First Quadrant");
+            }
+            if(x<0 && y>0)
+            {
+                Console.WriteLine("Points lies in second Quadrant");
+            }
+            if(x<0 && y<0)
+            {
+                Console.WriteLine("Points lies in Third Quadrant");
+            }
+            if(x>0 && y<0)
+            {
+                Console.WriteLine("Points lies in Fourth Quadrant");
+            }
+            if(x!=0 && y==0)
+            {
+                Console.WriteLine("Points lies in X-axis");
+
+            }
+            if(x==0 && y!=0)
+            {
+                Console.WriteLine("Points lies in y-axis");
+            }
+            if(x==0 && y==0)
+            {
+                Console.WriteLine("Points lies in Origin");
+            }
+        }
+    }
+}
+*/
