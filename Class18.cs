f7da60c095a55eeafc2da5e5cbca9333a7bae9bf
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:53:15 +0530

    Add files via upload

---
+++ Class18.cs
@@ -0,0 +1,30 @@
+﻿
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class18
+    {
+        public static void Main()
+        {
+            int a, b, c;
+            Console.WriteLine("Entter two number");
+            a=Convert.ToInt32(Console.ReadLine());  
+            b=Convert.ToInt32(Console.ReadLine());  
+            
+            c=a;
+            a = b;
+            b = c;
+            Console.WriteLine("A=" + a);
+            Console.WriteLine("A=" + b);
+          
+
+
+        }
+    }
+}
+*/
