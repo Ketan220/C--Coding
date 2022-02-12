720824212942d93b35cb11dae2dfb9213a00cb38
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:59:12 +0530

    Add files via upload

---
+++ Class43.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class43
+    {
+        public static void Main()
+        {
+            int i ,j;
+            for(i=1;i<=3;i++)
+            {
+                for(j=1; j<=4;j++)
+                { 
+                    Console.WriteLine(i);
+                    Console.WriteLine(j);
+                  
+                  
+                }
+
+            }
+        }
+    }
+}
