a9bcc546161cd502b54d6bff0d5df587a14a81f4
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:58:43 +0530

    Add files via upload

---
+++ Class41.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class39
+    {
+        public static void Main()
+        {
+            int num, d, sum = 0, i = 1, ori;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            ori = num;
+            while (num != 0) 
+            {
+                d = num % 10;
+                sum = sum*10 + d;
+                num = num / 10;
+            }
+            if(sum==ori)
+            {
+                Console.WriteLine("Number is Pallindrome");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Pallindrome");
+            }
+
+        }
+    }
+            
+}*/
+
