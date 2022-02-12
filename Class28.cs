47f6081f8ad9ef10518bc8a992e9316639b57af4
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:56:09 +0530

    Add files via upload

---
+++ Class28.cs
@@ -0,0 +1,25 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class28
+    {
+        public static void Main()
+        {
+            int ch;
+            Console.WriteLine("Enter character");
+            ch = Convert.ToChar(Console.ReadLine());
+            if (ch >= 'A' && ch <= 'Z')
+            {
+                ch = ch +32;
+            }
+            Console.WriteLine("Lowercase=" + ch);
+
+        }
+    }
+}*/
+
