c931d6f5f5bbfe6a09586b7bf9b8807e92f7799e
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:58:28 +0530

    Add files via upload

---
+++ Class40.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class39
+    {
+        public static void Main()
+        {
+            int num, d, sum = 0,i=1;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            while (num != 0) 
+            {
+                d = num % 10;
+                sum = sum*10 + d;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse=" + sum);
+        }
+    }
+            
+}*/
