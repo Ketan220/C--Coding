d8c63636a7ceed43a692657f6ae3820ad589c230
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:54:46 +0530

    Add files via upload

---
+++ Class23.cs
@@ -0,0 +1,32 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class23
+    {
+        public static void Main()
+        {
+            int num;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            if(num>0)
+            {
+                Console.WriteLine("No. is positive");
+            }
+            if(num<0)
+            {
+                Console.WriteLine("No. is Negative");
+            }
+            if (num == 0)
+            {
+                Console.WriteLine("No. is Zero");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+*/
