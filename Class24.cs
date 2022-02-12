36310ba0155504ba9fe80c7de4fe02a92008ca42
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:55:01 +0530

    Add files via upload

---
+++ Class24.cs
@@ -0,0 +1,31 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class24
+    {
+        public static void Main()
+        {
+            int a, b, c, max;
+            Console.WriteLine("Enter three numbers");
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            c=Convert.ToInt32(Console.ReadLine());
+            max = a;
+            if(b>max)
+            {
+                max = b;
+            }
+            if (c > max)
+            {
+                max = c;
+            }
+            Console.WriteLine("Largest=" + max);
+        }
+    }
+}
+*/
