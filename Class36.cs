efa4b85da0e502fe1feea2bb4c5e4413dbefc677
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:57:31 +0530

    Add files via upload

---
+++ Class36.cs
@@ -0,0 +1,25 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class36
+    {
+        public static void Main()
+        {
+            int num, i = 1, fact = 1;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            while(i<=num)
+            {
+                fact = fact * i;
+                i++;
+            }
+            Console.WriteLine("Factorial="+fact);
+        }
+    }
+}
+*/
