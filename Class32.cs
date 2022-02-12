55f245c82acab1a28f7645b9fabe3016fc0422f6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:32:36 +0530

    Add files via upload

---
+++ Class32.cs
@@ -0,0 +1,21 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class32
+    {
+        public static void Main()
+        {
+            int i = 1;
+            while(i<=10)
+            {
+                Console.WriteLine("sai compusys");
+                i++;
+            }
+        }
+    }
+}*/
