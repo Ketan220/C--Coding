7ee1bd2969bfc52de509a007ddc6d9e7bc261a8a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:58:00 +0530

    Add files via upload

---
+++ Class38.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+/**using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class38
+    {
+        public static void Main()
+        {
+            int x, y, p = 1, i = 1;
+            Console.WriteLine("Enter x and y");
+            x =Convert.ToInt32(Console.ReadLine());
+            y=Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=y;i++)
+            {
+                p = p * x;
+                
+            }
+            Console.WriteLine("Power="+p);
+        }
+    }
+}*/
