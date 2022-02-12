04df3d11b05ffb06e49247414d4be589a411feb6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 20:51:57 +0530

    Add files via upload

---
+++ Class34.cs
@@ -0,0 +1,23 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class34
+    {
+        public static void Main()
+        {
+            int i = 1, sum = 0;
+            while(i<=100)
+            {
+                sum = sum + i;
+                i++;
+            }
+            Console.WriteLine("Summation="+sum); 
+        }
+    }
+}
+*/
