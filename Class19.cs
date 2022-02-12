c818375f0307ed785b2006ceb96bed7cfbcc1267
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:53:30 +0530

    Add files via upload

---
+++ Class19.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class19
+    {
+        public static void Main()
+        {
+            float gms, kgs;
+            Console.WriteLine("Enter grams");
+            gms=Convert.ToSingle(Console.ReadLine());
+            kgs = gms / 1000;
+            Console.WriteLine("kilograms=" + kgs);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
+*/
+
