f43920de7afca1e2dd06dd69c521555bbd640ed2
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:54:30 +0530

    Add files via upload

---
+++ Class22.cs
@@ -0,0 +1,38 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class22
+    {
+        public static void Main()
+        {
+            float x1, x2, x3, y1, y2, y3, m1, m2;
+            Console.WriteLine("Enter the all values");
+            x1=Convert.ToSingle(Console.ReadLine());
+            x2=Convert.ToSingle(Console.ReadLine());
+            x3=Convert.ToSingle(Console.ReadLine());
+            y1=Convert.ToSingle(Console.ReadLine());
+            y2=Convert.ToSingle(Console.ReadLine());
+            y3=Convert.ToSingle(Console.ReadLine());
+            m1=Convert.ToSingle(Console.ReadLine());
+            m2=Convert.ToSingle(Console.ReadLine());
+
+            m1 = (y2 - y1) / (x2 - x1);
+            m2=(y3 - y1) / (x3 - x1);
+
+            if(m1==m2)
+            {
+                Console.WriteLine("Points are collinear");
+            }
+            else
+            {
+                Console.WriteLine("Points are not collinear");
+            }
+        }
+    }
+}
+*/
