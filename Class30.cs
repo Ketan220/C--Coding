8b0ce1092cda2bb7b2bf807bae2d088ee3b72a63
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:56:41 +0530

    Add files via upload

---
+++ Class30.cs
@@ -0,0 +1,62 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class30
+    {
+        public static void Main()
+        {
+            int mm;
+            Console.WriteLine("Enter month");
+            mm = Convert.ToInt32(Console.ReadLine());
+            switch(mm)
+            {
+                case 1:
+                    Console.WriteLine("january");
+                    break;
+                case 2:
+                    Console.WriteLine("Feburary");
+                    break;
+                case 3:
+                    Console.WriteLine("March");
+                    break;
+                case 4:
+                    Console.WriteLine("April");
+                    break;
+                case 5:
+                    Console.WriteLine("May");
+                    break;
+                case 6:
+                    Console.WriteLine("june");
+                    break;
+                case 7:
+                    Console.WriteLine("july");
+                    break;
+                case 8:
+                    Console.WriteLine("August");
+                    break;
+                case 9:
+                    Console.WriteLine("September");
+                    break;
+                case 10:
+                    Console.WriteLine("octomber");
+                    break;
+                case 11:
+                    Console.WriteLine("Novmber");
+                    break;
+                case 12:
+                    Console.WriteLine("Decmber");
+                    break;
+                default:
+                    Console.WriteLine("Invalid month");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}*/
