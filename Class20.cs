7a7891796a1c9e081cb7186bbc3f32cd71b81f9b
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-12 15:53:53 +0530

    Add files via upload

---
+++ Class20.cs
@@ -0,0 +1,31 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class20
+    {
+        public static void Main()
+        {
+            float amt, tamt=0;
+            int n;
+            Console.WriteLine("Enter the amount and no. of years");
+            amt=Convert.ToSingle(Console.ReadLine());
+            n=Convert.ToInt32(Console.ReadLine());
+            if(amt<10000)
+            {
+                tamt = amt + amt * 0.3f * n;
+            }
+            else
+            {
+                tamt = amt + amt * 0.18f * n;
+            }
+            Console.WriteLine("Total amt=" + tamt);
+
+         }
+    }
+}
+*/
