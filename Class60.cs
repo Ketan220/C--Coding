15fcfea21e6cfb76cc2e4e42ec1227b12011b103
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:04:42 +0530

    Add files via upload

---
+++ Class60.cs
@@ -0,0 +1,28 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class60
+    {
+        static void Main()
+        {
+            int num=5;
+            int res;
+            res = factorial(num);
+            Console.WriteLine("Factorial=" + res);
+        }
+        static int factorial(int num)
+        {
+            int i, fact = 1;
+            for (i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}*/
