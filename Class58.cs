d22fa9e9ef8d27941a39f8e5614017e5a9f9880f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:04:01 +0530

    Add files via upload

---
+++ Class58.cs
@@ -0,0 +1,33 @@
+﻿//16 wap to accept a number and pass this no. to method to print table of that no.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class57
+    {
+        
+        static void Main()
+        {
+            int num;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            factorial(num);
+            Console.ReadLine();
+        }
+        static void factorial(int x)
+        {
+            int i = 1;
+            int res = 1;
+            for(i=1; i<=10;i++)
+            {
+                res = i * x;
+                Console.WriteLine("{0} * {1} = {2}", x, i, res);
+                
+            }
+        }
+    }
+}*/
