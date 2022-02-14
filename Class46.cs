952cb174d54cf567d788d0b04a017dd2a90482de
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 20:58:17 +0530

    Add files via upload

---
+++ Class46.cs
@@ -0,0 +1,21 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class46
+    {
+        static void Main()
+        {
+            int i = 1;
+            for(i=1;i<=10;i++)
+            {
+                Console.WriteLine("I="+i);   
+            }
+            Console.ReadLine();
+        }
+    }
+}*/
