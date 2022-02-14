c57f2bb0a8b6f487afd79dc2b48cfb698cef653a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 20:59:09 +0530

    Add files via upload

---
+++ Class49.cs
@@ -0,0 +1,32 @@
+﻿//7-create a integer array of  3 elements and display the elements using for loop
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class49
+    {
+        static void Main()
+        {
+            int i;
+            int[] arr = new int[3];
+            arr[0] = 11;
+            arr[1] = 12;
+            arr[2] = 13;
+            for(i=0;i<3;i++)
+            {
+                Console.WriteLine("Arr="+arr[i]);
+            }
+
+            foreach(int i in arr)
+            {
+
+                Console.WriteLine("Arr=" + arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}*/
