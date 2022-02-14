4dbb09709c84d04faca6227ccb80fdf56a671061
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:03:26 +0530

    Add files via upload

---
+++ Class56.cs
@@ -0,0 +1,32 @@
+﻿//method with parameters parameters and returing no value
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class56
+    {
+        static void Main()
+        {
+            int num = 5;
+            Factorial(num);
+            Console.ReadLine();
+
+
+        }
+        static void Factorial(int x)
+
+
+        {
+            int i, fact = 1;
+            for (i = 1; i <= x; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial=" + fact);
+        }
+    }
+}*/
