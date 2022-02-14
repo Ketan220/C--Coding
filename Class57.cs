b6c752b99d76dacd7c6d0269d364b4b29e7770e8
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:03:42 +0530

    Add files via upload

---
+++ Class57.cs
@@ -0,0 +1,31 @@
+﻿//14 - wap to create a method to display table of a no.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class57
+    {
+        
+        static void Main()
+        {
+            int num = 5;
+            factorial(num);
+            Console.ReadLine();
+        }
+        static void factorial(int num)
+        {
+            int i = 1;
+            int res = 1;
+            for(i=1; i<=10;i++)
+            {
+                res = i * num;
+                Console.WriteLine("{0} * {1} = {2}", num, i, res);
+                
+            }
+        }
+    }
+}*/
