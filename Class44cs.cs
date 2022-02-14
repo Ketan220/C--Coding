595f78a1b374e857dbb2a499a639f9e903b582a8
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 20:57:36 +0530

    Add files via upload

---
+++ Class44cs.cs
@@ -0,0 +1,22 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class44cs
+    {
+        static void Main()
+        {
+            int i=1 ;
+            while(i<=10)
+            {
+                Console.WriteLine("I="+i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}*/
