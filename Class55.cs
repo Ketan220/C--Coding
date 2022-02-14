e89740dfa5e6037e6e736828314b37e0676a0ed3
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:03:07 +0530

    Add files via upload

---
+++ Class55.cs
@@ -0,0 +1,36 @@
+﻿//13 - wap to accept  a no. and pass this no. to method to display factorial
+//method with parameters parameters and returing no value
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class55
+    {
+        static void Main()
+        {
+            int num,res;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            Factorial(num);
+            Console.ReadLine();
+
+
+        }
+        static void Factorial(int x)
+            
+            
+        {
+            int i, fact = 1;
+            for(i=1;i<=x;i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial=" + fact);
+        }
+
+    }
+}*/
