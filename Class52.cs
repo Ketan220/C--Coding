b11751faef3bef6be9d36891df8b61c66d1fc16e
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:01:58 +0530

    Add files via upload

---
+++ Class52.cs
@@ -0,0 +1,28 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class52
+    {
+        static void Main()
+        {
+            int num;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0) goto even;
+            Console.WriteLine("No. is odd");
+            goto end;
+        even:
+            Console.WriteLine("No. is even");
+            end:;
+            
+
+        }
+        Console.ReadLine();
+        
+    }
+}*/
