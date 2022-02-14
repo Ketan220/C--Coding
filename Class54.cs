f861eaa9271d1af54270042983ca26622ad8c50e
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:02:51 +0530

    Add files via upload

---
+++ Class54.cs
@@ -0,0 +1,24 @@
+﻿//creating a user defined method displaygreeting
+//method with no parameters and returing no value
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class54
+    {
+        static void Main()
+        {
+            displaygreeting();
+        }
+        static void displaygreeting()
+        {
+            Console.WriteLine("Happy Valentine day");
+        }
+        Console.ReadLine();
+    }
+}*/
