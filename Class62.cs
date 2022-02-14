6ed9c0674b4c05beda01c11219b243b25353597a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:00:03 +0530

    Add files via upload

---
+++ Class62.cs
@@ -0,0 +1,32 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class62
+    {
+        static void Main()
+        {
+
+            int res;
+            res = factorial();
+            Console.WriteLine("Factorial=" + res);
+        }
+        static int factorial()
+
+        {
+            int i, fact = 1;
+            int num = 5;
+
+            num = Convert.ToInt32(num);
+            for (i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}*/
