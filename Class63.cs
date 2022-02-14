6873770d2e2fd8dba07446d3e732bad2a2535c1f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:01:21 +0530

    Add files via upload

---
+++ Class63.cs
@@ -0,0 +1,35 @@
+﻿//20 - example of local and shared variable
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class63
+    {
+        //class level variable
+        static int fact = 2; //shared variable
+
+      
+        static void factorial()
+        {
+            int fact = 1; // local variable 
+            Console.WriteLine("value of fact local varialbe inside method= " + fact);
+
+
+
+        }
+        static void Main()
+        {
+
+            factorial();
+            Console.WriteLine("fact inside main  = {0}", fact);
+            Console.ReadLine();
+
+
+        }
+
+    }
+}*/
