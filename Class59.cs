c301347ddb1ad3f7ffa6c5426703502cde58bf25
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:04:20 +0530

    Add files via upload

---
+++ Class59.cs
@@ -0,0 +1,32 @@
+﻿//method with parameters parameters and returing value
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class59
+    {
+        static void Main()
+        {
+            int num;
+            int res;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            res = factorial(num);
+            Console.WriteLine("Factorial=" + res);
+        }
+        static int factorial(int x)
+        {
+            int i, fact = 1;
+            for(i=1;i<=x;i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}
+*/
