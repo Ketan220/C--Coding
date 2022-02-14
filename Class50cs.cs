14fb4268590ff647c875923f753df22f33dc609f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 20:59:25 +0530

    Add files via upload

---
+++ Class50cs.cs
@@ -0,0 +1,30 @@
+﻿//for each loop
+//syntax foreach (datatype variable in arrayname) { //code }
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class50cs
+    {
+        static void Mian()
+        {
+
+            int i;
+            int[] arr = new int[3];
+            arr[0] = 11;
+            arr[1] = 12;
+            arr[2] = 13;
+            foreach(int i in arr)
+            {
+
+                Console.WriteLine("Arr=" + arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}*/
