8379ae534aec75f44a3d975a082eef720eb67590
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-14 21:02:25 +0530

    Add files via upload

---
+++ Class53.cs
@@ -0,0 +1,30 @@
+﻿//10 example of break and continue
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class53
+    {
+        static void Main()
+        {
+            int i = 1;
+            while(true)
+            {
+                Console.WriteLine(i);
+                i++;
+                if(i<=10)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();        }
+    }
+}*/
