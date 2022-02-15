592dd6e4777670ba54a1915ef8dcf2bb5dce10bb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:31:51 +0530

    Add files via upload

---
+++ Class71.cs
@@ -0,0 +1,30 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class71
+    {
+        static void Main()
+        {
+            char ch, r;
+            Console.WriteLine("Enter the lower case alphabet");
+            ch = Convert.ToChar(Console.ReadLine());
+
+
+            r = ucase(ch);
+            Console.WriteLine("Upper case=" + r);
+        }
+        static char ucase(char ch1)
+        {
+            if(ch1>=97 && ch1<=122)
+            {
+                ch1 = ch1 - 32;
+            }
+            return (ch1);
+        }
+    }
+}*/
