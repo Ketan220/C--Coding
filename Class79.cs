162035fec0d1cfb69b01823decbadc786119eeec
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:34:28 +0530

    Add files via upload

---
+++ Class79.cs
@@ -0,0 +1,30 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class79
+    {
+        static void Main()
+        {
+            int m1, m2, m3;
+            Console.WriteLine("Enter the marks of three subject");
+            m1=Convert.ToInt32(Console.ReadLine());
+            m2=Convert.ToInt32(Console.ReadLine());
+            m3=Convert.ToInt32(Console.ReadLine());
+            if(m1>=40 && m2>=40 && m3>40)
+            {
+                Console.WriteLine("Stident Pass");
+            }
+            else
+            {
+                Console.WriteLine("Student Failed");
+            }
+
+        }
+       
+    }
+}*/
