d54246fa0320d402ba62e556e1b5990d89ac4fe5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:37:04 +0530

    Add files via upload

---
+++ Class87.cs
@@ -0,0 +1,44 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class87
+    {
+        static void Main()
+        {
+            int x, y, pxy = 1, p26 = 1, p4x = 1, i = 1;
+            float s;
+            Console.WriteLine("Enter the value of x and y");
+            x=Convert.ToInt32(Console.ReadLine());
+            y=Convert.ToInt32(Console.ReadLine());
+            
+            while(i<=y)
+            {
+                pxy = pxy * x;
+                i++;
+            }
+            i = 1;
+            while(i<=6)
+            {
+                p26 = p26 * 2;
+                i++;
+            }
+            i = 1;
+            while(i<=x)
+            {
+                p4x = p4x * 4;
+                i++;
+            }
+            s = (pxy * p26) / (p4x * 0.1f);
+            Console.WriteLine("S="+s);
+            
+
+        }
+
+       
+    }
+}*/
