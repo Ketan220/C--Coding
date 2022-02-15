e2fa24a30c53e1deabfe5a53925753eec4aaaa6c
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 18:57:50 +0530

    Add files via upload

---
+++ Class94.cs
@@ -0,0 +1,25 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class94
+    {
+        struct time
+        {
+            public int hrs,min, sec;
+        }
+        static void Main()
+        {
+            time t1;
+            Console.WriteLine("Enter the time");
+            t1.hrs=Convert.ToInt32 (Console.ReadLine());
+            t1.min=Convert.ToInt32 (Console.ReadLine());
+            t1.sec=Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Time=" + t1.hrs + t1.min + t1.sec);
+        }
+    }
+}*/
