02734d1769c8fd56ba23ddb1c6b8bd94fdb33dbb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:36:44 +0530

    Add files via upload

---
+++ Class86.cs
@@ -0,0 +1,22 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class86
+    {
+        static void Main()
+        {
+            int i=1;
+            while(i<=100)
+            {
+                Console.WriteLine(i);
+                i = i * 3;
+            }
+        }
+    
+    }
+}*/
