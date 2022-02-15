b078f0c5c6fc0d055b87003804325d119267b0e7
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:34:14 +0530

    Add files via upload

---
+++ Class78.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class78
+    {
+        static void Main()
+        {
+            int num;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());    
+            if(num>=1 &&  num<=100)
+            {
+                Console.WriteLine("In the Range");
+            }
+            else
+            {
+                Console.WriteLine("Out of Range");
+            }
+        }
+    }
+}*/
