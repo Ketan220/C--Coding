bbfcbac7a67bb2ffabb2e29b0e99b8367377470e
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:30:47 +0530

    Add files via upload

---
+++ Class68.cs
@@ -0,0 +1,47 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class68
+    {
+        static void Main()
+        {
+            int a, b, c, d, e, r;
+            Console.WriteLine("Enter five numbers");
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            c=Convert.ToInt32(Console.ReadLine());
+            d=Convert.ToInt32(Console.ReadLine());
+            e=Convert.ToInt32(Console.ReadLine());
+            r = maximum(a, b, c, d, e);
+            Console.WriteLine("Largesdt=" + r);
+        }
+        static int  maximum(int a1,int b1,int c1,int d1,int e1)
+        {
+            int max;
+            max = a1;
+            if(b1>max)
+            {
+                max = b1;
+            }
+            if(c1>max)
+            {
+                max=c1;
+            }
+            if(d1>max)
+            {
+                max = d1;
+            }
+            if(e1>max)
+            {
+                max = e1;
+            }
+            return max;
+        }
+     
+    }
+}*/
