f64911c5f015203b756c7ccf065472af74e3eee0
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:36:15 +0530

    Add files via upload

---
+++ Class84.cs
@@ -0,0 +1,20 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class84
+    {
+        static void Main()
+        {
+            int i;
+            for(i=10;i>=1;i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}*/
