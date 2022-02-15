6be788e485fe034bf7d1bc4cd9a88c6b12e40238
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:33:22 +0530

    Add files via upload

---
+++ Class75.cs
@@ -0,0 +1,33 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class75
+    {
+        static void Main()
+        {
+            int d1, d2, num, sum = 0, ori;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            ori = num;
+            d1 = num % 10;
+            num = num / 10;
+            d2 = num % 10;
+            num = num / 10;
+            sum = d1 *100 + d2 * 10+ num ;
+            if (sum == ori)
+            {
+                Console.WriteLine("No. is Pallindrome");
+            }
+            else
+            {
+                Console.WriteLine("No. is not Pallindrome");
+            }
+        }
+    }
+}*/
+
