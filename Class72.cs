092711b63276cddb59ccbc4dd7b88bd9f9806061
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:32:17 +0530

    Add files via upload

---
+++ Class72.cs
@@ -0,0 +1,32 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class72
+    {
+        static void Main()
+        {
+            char ch;
+            int num;
+            
+            Console.WriteLine("Enter character and no.");
+            ch=Convert.ToChar(Console.ReadLine());
+            num=Convert.ToInt32(Console.ReadLine());
+            display(ch, num);
+            
+
+        }
+        static void display(char ch1,int n)
+        {
+            int i;
+            for(i=1;i<=n;i++)
+            {
+                Console.Write(ch1+"\t");   
+            }
+        }
+    }
+}*/
