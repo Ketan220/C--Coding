9023f63f253d7d50bfd151b6bc850e41cc249756
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:33:00 +0530

    Add files via upload

---
+++ Class74.cs
@@ -0,0 +1,32 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class74
+    {
+        static void Main()
+        {
+            int d1, d2, num, sum = 0, ori;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            ori = num;
+            d1 = num % 10;
+            num = num / 10;
+            d2 = num % 10;
+            num = num / 10;
+            sum = d1 * d1 * d1 + d2 * d2 * d2 + num * num * num;
+            if(sum==ori)
+            {
+                Console.WriteLine("No. is Amstrong");
+            }
+            else
+            {
+                Console.WriteLine("No. is not amstrong");
+            }
+        }
+    }
+}*/
