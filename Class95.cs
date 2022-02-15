67fa666f87da878923b765ca0292d152aa18a553
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 18:58:04 +0530

    Add files via upload

---
+++ Class95.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class95
+    {
+        struct measure
+        {
+            public int feet,inches;
+        }
+        static void Main()
+        {
+            measure m;
+            Console.WriteLine("Enter feet and inches");
+            m.feet = Convert.ToInt32(Console.ReadLine());
+            m.inches= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Measure=" + m.feet + m.inches);
+
+        }
+    }
+}
