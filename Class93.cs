1807593034fcbeca3ba52bbeec69764ece19dbd7
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 18:57:32 +0530

    Add files via upload

---
+++ Class93.cs
@@ -0,0 +1,25 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class93
+    {
+        struct date
+        {
+            public int dd,mm,yy;
+        }
+        static void Main()
+        {
+            date d1;
+            Console.WriteLine("Enter date month year");
+            d1.dd=Convert.ToInt32(Console.ReadLine());
+            d1.mm=Convert.ToInt32(Console.ReadLine());
+            d1.yy=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Date=" + d1.dd + d1.mm + d1.yy);
+        }
+    }
+}*/
