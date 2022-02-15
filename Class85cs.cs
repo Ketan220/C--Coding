e2ed850db2ad9500e3bd1c29d9c1e35e7cd239e3
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:36:30 +0530

    Add files via upload

---
+++ Class85cs.cs
@@ -0,0 +1,21 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class85cs
+    {
+        static void Main()
+        {
+            int i = 1;
+            while(i<=10)
+            {
+                Console.WriteLine(i);
+                i = i + 3;
+            }
+        }
+    }
+}*/
