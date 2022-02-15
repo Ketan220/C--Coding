9d322de2ff62df19bb1159f18236de187aadbfa2
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:37:24 +0530

    Add files via upload

---
+++ Class88.cs
@@ -0,0 +1,30 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class88
+    {
+        static void Main()
+        {
+            int num, digit, d, count = 0;
+            Console.WriteLine("Enter the number and digit");
+            num = Convert.ToInt32(Console.ReadLine());
+            digit = Convert.ToInt32(Console.ReadLine());
+            if(num!=0)
+            {
+                d = num % 10;
+                if(d==digit)
+                {
+                    count++;
+                }
+                num = num / 10;
+            }
+            Console.WriteLine("Count=" + count);
+
+        }
+    }
+}*/
