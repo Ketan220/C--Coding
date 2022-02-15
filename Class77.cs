95970467605f31e7e289444b78e9a1a91e2e7821
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:33:54 +0530

    Add files via upload

---
+++ Class77.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class77
+    {
+        static void Main()
+        {
+            int feet, inches;
+            Console.WriteLine("Enter feet and inches");
+            feet=Convert.ToInt32(Console.ReadLine());   
+            inches=Convert.ToInt32(Console.ReadLine());
+            inches = inches + 1;
+            if(inches>=12)
+            {
+                feet = feet + inches / 12;
+                inches = inches % 10;
+            }
+            Console.WriteLine("New feet and inchess="+feet + inches);
+        }
+    }
+}*/
