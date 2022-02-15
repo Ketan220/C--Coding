2d2f1c8fd54136697a7d6c44aae20118ccfa80ba
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:37:43 +0530

    Add files via upload

---
+++ Class89.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class89
+    {
+        static void Main()
+        {
+            int num, d, sum = 0, w = 1;
+            Console.WriteLine("Enter Binary no.");
+            num=Convert.ToInt32(Console.ReadLine());
+            while(num!=0)
+            {
+                d = num % 10;
+                sum = sum + d * w;
+                w = w * 2;
+                num = num / 10;
+            }
+            Console.WriteLine("Decimal=" + sum);
+        }
+    }
+}*/
