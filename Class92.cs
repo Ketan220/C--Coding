62a5af7b817a185fde6e4084bd1b88545bddfc71
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 18:57:19 +0530

    Add files via upload

---
+++ Class92.cs
@@ -0,0 +1,33 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class92
+    {
+        struct student
+        {
+            public int rno;
+            public string name;
+            public float bs;
+        }
+        static void Main()
+        {
+            student s1;
+            Console.WriteLine("enter rno name and bs");
+            s1.rno = Convert.ToInt32(Console.ReadLine());
+            s1.name = Console.ReadLine();
+            s1.bs = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Roll No=" + s1.rno);
+            Console.WriteLine("Name="+s1.name);
+            Console.WriteLine("Bs="+s1.bs);
+
+
+
+        }
+
+    }
+}*/
