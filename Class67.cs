7f67a8f02c2202097a848d9889aec6f8741a6681
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:30:29 +0530

    Add files via upload

---
+++ Class67.cs
@@ -0,0 +1,29 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class67
+    {
+        static void Main()
+        {
+            int a, b, r;
+            Console.WriteLine("Enter numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            r = add(a, b);
+            Console.WriteLine("Substraction=" + r);
+
+        }
+        static int add(int x, int y)
+        {
+            int s;
+            s = x -y;
+            return (s);
+        }
+
+    }
+}*/
