042d2b9181ee8420f541386fa000e2fdc35ea9c7
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:32:44 +0530

    Add files via upload

---
+++ Class73.cs
@@ -0,0 +1,31 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class73
+    {
+        static void Main()
+        {
+            int bytes, bits;
+            float kb, mb, gb;
+            Console.WriteLine("Enter no of bytes");
+            bytes=Convert.ToByte(Console.ReadLine());
+            bits=Convert.ToInt32(Console.ReadLine());
+            kb=Convert.ToSingle(Console.ReadLine()); 
+            mb=Convert.ToSingle(Console.ReadLine()); 
+            gb=Convert.ToSingle(Console.ReadLine()); 
+            bits = bytes * 8;
+            kb = bytes / 1024.0f;
+            mb = kb / 1024;
+            gb = mb / 1024;
+            Console.WriteLine("Bits=" + bits);
+            Console.WriteLine("kb" + kb);
+            Console.WriteLine("mb=" + mb);
+            Console.WriteLine("gb=" + gb);
+        }
+    }
+}*/
