67faa28f8148fbc45a19dfb4273a29d17594bf7c
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:38:13 +0530

    Add files via upload

---
+++ Class91.cs
@@ -0,0 +1,23 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class91
+    {
+        static void Main()
+        {
+            int i, j;
+            for (i= 1;i<= 5;i++)
+            {
+               for(j=1;j<=5;j++)
+                {
+                    Console.Write(j);
+                }
+            }
+        }
+    }
+}*/
