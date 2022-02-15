a1842a672fbc5ad7770375c4fcce37fed27f5e5d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:31:02 +0530

    Add files via upload

---
+++ Class69.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class69
+    {
+        static void Main()
+        {
+            int a, r;
+            Console.WriteLine("Enter number");
+            a = Convert.ToInt32(Console.ReadLine());
+            r = Square(a);
+            Console.WriteLine("Square=" + r);
+        }
+        static int Square(int x)
+        {
+            int s1;
+            s1 = x * x;
+            return s1;
+        }
+    }
+}*/
