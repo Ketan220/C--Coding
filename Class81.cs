134f46b4c9770eceefaa9af4063b390cdacc2084
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-15 16:35:05 +0530

    Add files via upload

---
+++ Class81.cs
@@ -0,0 +1,35 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class81
+    {
+        static void Main()
+        {
+            char ch;
+            Console.WriteLine("Enter the character");
+            ch=Convert.ToChar(Console.Read());
+            if(ch>=65 && ch<=91)
+            {
+                Console.WriteLine("Character is upper case aplhabet");
+
+            }
+            if(ch>=97 && ch<=122)
+            {
+                Console.WriteLine("Character is lower case alphabet");
+            }
+            if(ch>=48 && ch<=57)
+            {
+                Console.WriteLine("Character is a digit");
+            }
+            if((ch>=0 && ch<=47)|| (ch>=58 && ch<=64) || (ch>=123 && ch<=255))
+            {
+                Console.WriteLine("Character is a special Symbol");
+            }
+        }
+    }
+}*/
