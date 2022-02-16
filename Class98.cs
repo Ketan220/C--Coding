209f7dc6c0ac0af915b1c7f0d4789acd4b15e0f3
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:51:53 +0530

    Add files via upload

---
+++ Class98.cs
@@ -0,0 +1,25 @@
+﻿//3- example of out 
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class198cs
+    {
+        static void Main()
+        {
+            int roi;
+            add(out roi);
+            Console.WriteLine("Value=" + roi);
+        }
+        static void add(out int x)
+        {
+            x = 5;
+            x++;
+            Console.WriteLine("value=" + x);
+        }
+    }
+}*/
