4421162355c6dbb5e279b364374ce3781e03bcde
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:54:56 +0530

    Add files via upload

---
+++ Class108.cs
@@ -0,0 +1,32 @@
+﻿//example of array spicifying length at run time
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class108
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number");
+            string s=Console.ReadLine();
+            int size=int.Parse(s);
+            long[] row = new long [size];
+            int i = 0;
+            for(i=0;i<row.Length;i++)
+            {
+                Console.Write("Enter numbers");
+                row[i]=Convert.ToInt32 (Console.ReadLine());
+
+            }
+
+            for(i=0;i<row.Length;i++)
+            {
+                Console.WriteLine(" " + row[i]);
+            }
+        }
+    }
+}*/
