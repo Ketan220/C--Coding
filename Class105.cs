e68e33c7912f1c0d92a390c996f33cd452891e8a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:54:10 +0530

    Add files via upload

---
+++ Class105.cs
@@ -0,0 +1,23 @@
+﻿//9 example of declaring and initilizing an array
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class105
+    {
+        static void Main()
+        {
+            long[] arr = new long[4] { 1, 2, 3, 4};
+            int i;
+            for(i=0;i<4;i++)
+            {
+                Console.Write("\t"+arr[i]);
+            }
+
+        }
+    }
+}*/
