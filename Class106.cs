adc955628c6b5eb69f208ce4bb5addde4c0d2ce5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:54:25 +0530

    Add files via upload

---
+++ Class106.cs
@@ -0,0 +1,23 @@
+﻿//example of single dimension array
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class106
+    {
+        static void Main()
+        {
+            long[] row = { 1, 2, 3, 4, 5 };
+            int i;
+            for(i=0;i<6;i++)
+            {
+                Console.Write("\t"+row[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}*/
