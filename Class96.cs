1b1b654cc0279be48f8233e0281b8701db9ff467
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:50:42 +0530

    Add files via upload

---
+++ Class96.cs
@@ -0,0 +1,25 @@
+﻿//1- example of pass by value
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class96
+    {
+        static void Main()
+        {
+            int k = 6;
+            add(k);
+            Console.WriteLine(k);
+
+        }
+        static void add(int x)
+        {
+            x++;
+            Console.WriteLine("value inside add method is ="+x);
+        }
+    }
+}*/
