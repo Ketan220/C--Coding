a13565fde39184b6c8e3232ddbc0137e14dfe572
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:56:04 +0530

    Add files via upload

---
+++ Class112.cs
@@ -0,0 +1,33 @@
+﻿//17  example of method returning an array
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class112
+    {
+        static void Main()
+        {
+            int[] arr = CreateArray(3);
+            for (int i=0; i<arr.Length; i++)
+            {
+                Console.WriteLine("Array="+arr[i]);
+            }
+
+
+        }
+        static int[] CreateArray(int size)
+        {
+            int[] created=new int [size];
+            created[0]=1;
+            created[1]=2;
+            created[2]=3;
+            return created;
+
+        }
+        
+    }
+}*/
