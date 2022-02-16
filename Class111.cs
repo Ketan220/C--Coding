880b8cac327a0f7a593aefce2376e571dc252bfd
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:55:50 +0530

    Add files via upload

---
+++ Class111.cs
@@ -0,0 +1,24 @@
+﻿//16 example of array properties
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class111
+    {
+        static void Main()
+        {
+            long[] row = new long[4] { 1, 2, 3, 4 };
+            Console.WriteLine("Array Rank="+row.Rank);
+            Console.WriteLine("Array Length="+row.Length);
+
+
+            long[,] row1 = new long[2, 4];
+            Console.WriteLine("Array Rank="+row1.Rank);
+            Console.WriteLine("Array Length="+row1.Length);
+        }
+    }
+}*/
