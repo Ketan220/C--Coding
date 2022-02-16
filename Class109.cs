cb87c3dafa1d4c0ba7bc4649abbc96487d6eeef5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:55:15 +0530

    Add files via upload

---
+++ Class109.cs
@@ -0,0 +1,28 @@
+﻿//15 example of copy array variable 
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class109
+    {
+        static void Main()
+        {
+            long [] row=new long [4] { 1,2,3,4};
+            long[] copy = row;
+            row[0]++;
+            long value = copy[0];
+            Console.WriteLine("value=" +"\n");
+
+            for (int i= 0;i < copy.Length;i++)
+            {
+                Console.WriteLine(" " + copy[i]);
+            }
+
+
+        }
+    }*/
+}
