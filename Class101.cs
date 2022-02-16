7d74328c2d74db410fb64d7771cdd511109a0296
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:52:55 +0530

    Add files via upload

---
+++ Class101.cs
@@ -0,0 +1,29 @@
+﻿//6  example of method overloading
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class101
+    {
+        static void Main()
+        {
+            int x, y;
+            x = add(1, 2);
+            y = add( 2, 3);
+            Console.WriteLine("Addition of 2 value=" + x);
+            Console.WriteLine("Addition of 3 value=" + y);
+        }
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+        static long add(int a, long b)
+        {
+            return a + b ;
+        }
+    }
+}*/
