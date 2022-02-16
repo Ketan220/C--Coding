842262e0738f140a76e53765b4353ecc1bd150ad
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:53:13 +0530

    Add files via upload

---
+++ Class102.cs
@@ -0,0 +1,31 @@
+﻿//example of single dimension array
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class102
+    {
+        static void Main()
+        {
+            int[] arr = new int[3];
+            int i=0;
+            for (i = 0; i <3; i++)
+            {
+                Console.WriteLine("Enter marks");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(i=0;i<=arr.Length;i++)
+            {
+                Console.WriteLine("Marks="+arr[i]);
+            }
+
+
+
+
+        }
+    }
+}*/
