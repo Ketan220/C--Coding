591442a983f3b2d00da48473696d0e81649032dd
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:52:12 +0530

    Add files via upload

---
+++ Class99.cs
@@ -0,0 +1,27 @@
+﻿//4- example of varialbe lenght parameter list
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class99
+    {
+      static void Main()
+        {
+            long x =Addlist(1, 2, 3, 4, 5 );
+            Console.WriteLine("Value=" + x);
+        }
+        static long Addlist(params long[] v)
+        {
+            long total=0, i;
+            for(i=0;i<v.Length;i++)
+            {
+                total = total+v[i];
+            }
+            return total;
+        }
+    }
+}*/
