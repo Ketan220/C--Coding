2007a214abca4e3bb9d1fd6e47f84719d8d69edb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:54:40 +0530

    Add files via upload

---
+++ Class107.cs
@@ -0,0 +1,30 @@
+﻿//11- example of declaring and initilizing double dimension array of 2 rows and 3 col
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class107
+    {
+        static void Main()
+        {
+            int[,] grid = { { 5, 4, 3 }, { 2, 1, 0 } };
+
+
+            //displaying the array elements
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("\t " + grid[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}*/
