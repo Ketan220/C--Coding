30ca463ab6168d3ce5998a1f161451312d58af9a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:56:25 +0530

    Add files via upload

---
+++ Class113.cs
@@ -0,0 +1,28 @@
+﻿//18 example of passing array to method
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class113
+    {
+        static void Main()
+        {
+            int[] created = new int[3];
+            created[0] = 1;
+            created[1] = 2;
+            created[2] = 3;
+             createdarray(created);
+        }
+        static void createdarray(int[] array)
+        {
+            for(int i=0; i<array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
