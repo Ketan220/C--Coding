f0c6ba48af8c288e4044da5ecfad0ddeb9544012
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-16 20:51:18 +0530

    Add files via upload

---
+++ Class97.cs
@@ -0,0 +1,24 @@
+﻿//2- example of pass by reference
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class97
+    {
+        static void Main()
+        {
+            int k = 6;
+            add(ref k);
+            Console.WriteLine(k);
+        }
+        static void add(ref int x)
+        {
+            x++;
+            Console.WriteLine("Value="+x);
+        }
+    }
+}*/
