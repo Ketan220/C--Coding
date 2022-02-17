daaf25be80a8fa567d3166c0597431c22a24c367
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-17 21:42:29 +0530

    Add files via upload

---
+++ Class118.cs
@@ -0,0 +1,45 @@
+﻿//4- pass the data to class methods
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Employee
+    {
+        int empid;
+        string empname;
+        float empsalary;
+
+        public void getdata(int a,string b,float c)
+        {
+            empid = a;
+            empname = b;
+            empsalary = c;
+        }
+
+        public void showdata()
+        {
+            Console.WriteLine("Empid="+empid);
+            Console.WriteLine("Empname="+empname);
+            Console.WriteLine("Empsalary="+empsalary);
+        }
+    }
+    internal class Class118
+    {
+       static void Main()
+        {
+            Employee emp = new Employee();
+
+            Console.WriteLine("Enter empid,empname and empsalry");
+            int ei=Convert.ToInt32(Console.ReadLine());
+            string en=Console.ReadLine();
+            float es=Convert.ToInt32(Console.ReadLine());
+
+            emp.getdata(ei, en, es);
+            emp.showdata();
+        }
+    }
+}*/
