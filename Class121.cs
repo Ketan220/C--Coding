e5b88ea8be564ed5b01589e584b554b3d69cd150
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-17 21:43:23 +0530

    Add files via upload

---
+++ Class121.cs
@@ -0,0 +1,27 @@
+﻿//2- create a class cirlce having fields radius and method calculatearea and calculatecirc
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class circle
+    {
+        float r;
+        
+    }
+
+    }
+    internal class Class121
+    {
+      static  void Mian()
+    {
+        circle c = new circle();
+
+    }
+       
+    }
+*/
+
