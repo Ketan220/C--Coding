69382b11c66ee86bea22117a9cdd87a4110a5e3f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-17 21:41:45 +0530

    Add files via upload

---
+++ Class116.cs
@@ -0,0 +1,35 @@
+﻿//create  a class employee having fields empno,empname and salary
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public float empsalary;
+    }
+    internal class Class116
+    {
+        static void Main()
+        {
+            //creating an object(instance) of employee class
+            Employee emp = new Employee();
+
+            Console.WriteLine("Enter empno,empname and empsalary");
+            emp.empno=Convert.ToInt32(Console.ReadLine());
+            emp.empname=Console.ReadLine();
+            emp.empsalary=Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Empno=" + emp.empno);
+            Console.WriteLine("Empname=" + emp.empname);
+            Console.WriteLine("Empsalary=" + emp.empsalary);
+
+
+        }
+    }
+}*/
