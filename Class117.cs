483b54c8cb9ff00160b4087291bceb6aa58e1192
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-17 21:42:11 +0530

    Add files via upload

---
+++ Class117.cs
@@ -0,0 +1,41 @@
+﻿//3 - create a class employee having fields and methods getdata and showdata
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Employee
+    {
+        int empno;
+        string empname;
+        float empsalary;
+
+        public void getdata()
+        {
+            Console.WriteLine("Enter empno,empname and empsalary");
+            empno =Convert.ToInt32(Console.ReadLine());
+            empname= Console.ReadLine();
+            empsalary=Convert.ToSingle(Console.ReadLine());
+
+        }
+        public  void showdata()
+        {
+            Console.WriteLine("Empno="+empno);
+            Console.WriteLine("Empno="+empname);
+            Console.WriteLine("Empno="+empsalary);
+        }
+    }
+    internal class Class117
+    {
+        static void Main()
+        {
+            Employee emp = new Employee();
+
+            emp.getdata();
+            emp.showdata();
+        }
+    }
+}*/
