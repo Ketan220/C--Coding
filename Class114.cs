b421429ff29bd34445260f14e2031f7b74ca470b
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-17 21:40:53 +0530

    Add files via upload

---
+++ Class114.cs
@@ -0,0 +1,31 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    //create  a class employee having fields empno,empname and salary
+    
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public float salary;
+    }
+    internal class Class114
+    {
+        static void Main()
+        {
+            Employee emp = new Employee();
+            emp.empno = 121;
+            emp.empname = "ketan";
+            emp.salary = 12000;
+
+            Console.WriteLine("Empno.=" + emp.empno);
+            Console.WriteLine("Empname=" + emp.empname);
+            Console.WriteLine("Salary=" + emp.salary);
+        }
+    }
+}*/
