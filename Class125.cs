b5fc103f67d99ee2f6962bf04be8719331eac31e
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-18 18:38:29 +0530

    Add files via upload

---
+++ Class125.cs
@@ -0,0 +1,42 @@
+﻿/*1 - create a class book having member variables bookid , title, author, and price 
+and a constructor with no parameters and method display.*/
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Book
+    {
+        int bookid;
+        string title, author;
+        int price;
+
+        public Book()
+        {
+            bookid = 1;
+            title = "Java";
+            author ="Muzahid sir";
+            price = 2000;
+        }
+        public void display()
+        {
+            Console.WriteLine("Bookid="+bookid );
+            Console.WriteLine("Booktitle="+title);
+            Console.WriteLine("Bookauthor="+author);
+            Console.WriteLine("Bookprice="+price);
+        }
+
+    }
+    internal class Class125
+    {
+       static void Main()
+        {
+            Book b = new Book();
+
+            b.display();
+        }
+    }
+}*/
