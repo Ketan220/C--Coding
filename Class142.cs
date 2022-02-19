dc5bfc01d3f193c74eef84b65d140b177df716ac
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-19 21:41:19 +0530

    Add files via upload

---
+++ Class142.cs
@@ -0,0 +1,38 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class142
+    {
+        static void Main()
+        {
+            int[] arr = new int[10];
+            int i, num, flag = 0;
+            for(i=0;i<=9;i++)
+            {
+                Console.WriteLine("Enter elements");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Enter the number to be searched in the array");
+            num=Convert.ToInt32(Console.ReadLine());
+            for(i=0;i<=9; i++)
+            {
+                if(arr[i]==num)
+                {
+                    Console.WriteLine("Number is preasent");
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0)
+            {
+                Console.WriteLine("Number is not present");
+            }
+           
+        }
+    }
+}*/
