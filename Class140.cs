2a58a92a391e9275c3486ff9dc8714a4614fbd00
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-19 21:38:39 +0530

    Add files via upload

---
+++ Class140.cs
@@ -0,0 +1,29 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class140
+    {
+        static void Main()
+        {
+            int num, sum = 0, count = 0;
+            float avg;
+            char another = 'y';
+            while(another =='y')
+            {
+                Console.WriteLine("Enter number");
+                num=Convert.ToInt32(Console.ReadLine());
+                sum = sum + num;
+                count++;
+                Console.WriteLine("Do you wish to continue[y/n}?");
+                another=Convert.ToChar(Console.ReadLine());
+            }
+            avg = sum / count * 0.1f;
+            Console.WriteLine("Average=" + avg);
+        }
+    }
+}*/
