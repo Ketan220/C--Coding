00960c67fb4296f7003269522a41ec535c814654
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-19 21:35:07 +0530

    Add files via upload

---
+++ Class136.cs
@@ -0,0 +1,37 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class136
+    {
+        static void Main()
+        {
+            int i, num, count = 0;
+            int[] arr = new int[10];
+            for (i = 0; i <= 9; i++)
+            {
+                Console.WriteLine("Enter numbers");
+                arr[i] =Convert.ToInt32(Console.ReadLine ());
+
+            }
+            Console.WriteLine("ENTER ANOTHER NUMBERS");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            for(i=0;i<=9;i++)
+            {
+                if(arr[i]==num)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Count="+count);
+        }
+       
+
+
+    }
+}*/
