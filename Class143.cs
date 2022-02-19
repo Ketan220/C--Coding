f0ddab0d1d89c75ac16c10fbf324bc892ada129b
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-19 21:42:16 +0530

    Add files via upload

---
+++ Class143.cs
@@ -0,0 +1,34 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class143
+    {
+        static void Main()
+        {
+            int[,] a = new int[3, 4];
+            int i=0, j=0;
+            for(i=0;i<=2;i++)
+            {
+                for(j=0;j<=3;j++)
+                {
+                    Console.WriteLine("Enter elements");
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (i = 0; i <=2; i++)
+            {
+                for (j = 0; j <=3; j++)
+                {
+                    Console.Write("\t"+a[i,j]);
+                   
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}*/
