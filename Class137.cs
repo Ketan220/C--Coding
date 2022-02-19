6d22ecbcc81eab7b07c96d07c9ba93a77b1155d8
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-19 21:35:24 +0530

    Add files via upload

---
+++ Class137.cs
@@ -0,0 +1,34 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class137
+    {
+       static void Main()
+        {
+            int max, i, pos;
+            int[] arr = new int[10];
+            for(i=0;i<=9;i++)
+            {
+                Console.WriteLine("Enter number");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            max = arr[0];
+            pos = 0;
+            for(i=0;i<=9;i++)
+            {
+                if(arr[i]>max)
+                {
+                    max= arr[i];
+                    pos = i;
+                }
+            }
+            Console.WriteLine("Largest=" + max);
+            Console.WriteLine("Position=" + pos);
+        }
+    }
+}*/
