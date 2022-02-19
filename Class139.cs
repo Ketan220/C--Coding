8cebe7b32c7ee2b89fc641537633d975583e6df7
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-19 21:37:58 +0530

    Add files via upload

---
+++ Class139.cs
@@ -0,0 +1,29 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class139
+    {
+       static void Main()
+        {
+            int i, n, num, sum = 0;
+            float avg;
+            Console.WriteLine("kitane numbers");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            for(i = 0; i <n; i++)
+            {
+                Console.WriteLine("Enter numbers");
+                num=Convert.ToInt32(Console.ReadLine());
+                sum = sum + num;
+            }
+            avg = sum / n * 1.0f;
+
+            Console.WriteLine("Average=" + avg);
+        }
+    }
+}*/
