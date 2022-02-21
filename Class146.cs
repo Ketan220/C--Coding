95b32f962bc3143e1a24ba0aef6d6e6177c1ede4
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:22:58 +0530

    Add files via upload

---
+++ Class146.cs
@@ -0,0 +1,29 @@
+﻿//2) C# Protected Access Specifier
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class ProtectedTest
+    {
+        protected string name = "Shashikant";
+        protected void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+    }
+    internal class class146
+    {
+        static void Main(string[] args)
+        {
+            ProtectedTest protectedTest = new ProtectedTest();
+            // Accessing protected variable  
+            Console.WriteLine("Hello " + protectedTest.name);
+            // Accessing protected function  
+            protectedTest.Msg("Swami Ayyer");
+        }
+    }
+}*/
