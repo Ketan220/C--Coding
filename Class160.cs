f3e3ea580d4efc17d359362f412888b3403c11e1
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:27:48 +0530

    Add files via upload

---
+++ Class160.cs
@@ -0,0 +1,42 @@
+﻿//Let's see the example of multi level inheritance in C#.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating.....");
+        }
+    }
+    public class dog:animal
+    {
+        public void bark()
+        {
+            Console.WriteLine("Barking......");
+        }
+    }
+    public class babydog : dog
+    {
+        public void weep()
+        {
+            Console.WriteLine("Weeping........");
+        }
+    }
+    internal class Class160
+    {
+        public static void Main()
+        {
+            babydog b1 = new babydog();
+            b1.eat();
+            b1.bark();
+            b1.weep();
+
+        }
+    }
+}*/
