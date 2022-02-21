bf1aeeda99176b40d899f5537bbc09be915e889e
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:23:13 +0530

    Add files via upload

---
+++ Class147.cs
@@ -0,0 +1,29 @@
+﻿//Here, we are accessing protected members within child class by inheritance.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+        class ProtectedTest  
+        {  
+            protected string name = "Shashikant";  
+            protected void Msg(string msg)  
+            {  
+                Console.WriteLine("Hello " + msg);  
+            }  
+        }  
+        class Program : ProtectedTest  
+        {  
+            static void Main(string[] args)  
+            {  
+                Program program = new Program();  
+                // Accessing protected variable  
+                Console.WriteLine("Hello " + program.name);  
+                // Accessing protected function  
+                program.Msg("Swami Ayyer");  
+            }  
+        }      
+ }*/
