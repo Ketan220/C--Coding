4ba538e65af5d575830c6ada7da264dcd99e23e0
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:26:14 +0530

    Add files via upload

---
+++ Class155.cs
@@ -0,0 +1,34 @@
+﻿/*C# Single Level Inheritance Example: Inheriting Methods
+Let's see another example of inheritance in C# which inherits methods only.*/
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating.....");
+        }
+    }
+    public class dog:animal
+    {
+        public void bark()
+        {
+        Console.WriteLine("Barking....");
+        }
+    }
+    internal class Class155
+    {
+       public static void Main()
+        {
+            dog d1=new dog();
+            d1.eat();
+            d1.bark();
+        }
+    }
+}*/
