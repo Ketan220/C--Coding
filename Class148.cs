f538a8535bbf0b8540346d3d2f855c403b903cf7
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:23:24 +0530

    Add files via upload

---
+++ Class148.cs
@@ -0,0 +1,29 @@
+﻿//3) C# Internal Access Specifier
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class InternalTest
+    {
+        internal string name = "Abishekh Nalwale";
+
+        internal void Msg(string msg)
+        {
+            Console.WriteLine("Hello="+msg);
+        }
+    }
+    internal class Class148
+    {
+        static void Main()
+        {
+            InternalTest InternalTest = new InternalTest();
+            Console.WriteLine("Name=" + InternalTest.name);
+            InternalTest.Msg("Sans");
+        }
+
+    }
+}*/
