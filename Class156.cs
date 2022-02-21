a97ca4540e3633cd42ff28d5a4baa96204c1ca64
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:26:48 +0530

    Add files via upload

---
+++ Class156.cs
@@ -0,0 +1,54 @@
+﻿//another example 
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class person
+    {
+        string name;
+        string address;
+        public void getpersondata(string name,string address)
+        {
+            this.name = name;   
+            this.address = address; 
+        }
+        public void getdisplaypersondata()
+        {
+            Console.WriteLine("Name="+this.name);
+            Console.WriteLine("Address=" + this.address);
+
+        }
+    }
+
+    public class student : person
+    {
+        int rno;
+        int marks;
+
+        public void getstudentdata(int rno, int marks)
+        {
+            this.rno = rno;
+            this.marks = marks;
+        }
+        public void getdiplaystudentdata()
+        {
+            Console.WriteLine("Rno=" + this.rno);
+            Console.WriteLine("Marks"+this.marks);
+        }
+    }
+    internal class Class156
+    {
+        public static void Main()
+        {
+            student s1=new student();
+            s1.getpersondata("utkarsha", "Sakardara");
+            s1.getdisplaypersondata ();
+            s1.getstudentdata(1, 12);
+            s1.getdiplaystudentdata();
+        }
+    }
+}*/
