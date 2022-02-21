70c246d6465f24a48737246dc5c1a045329f56ed
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:25:07 +0530

    Add files via upload

---
+++ Class152.cs
@@ -0,0 +1,29 @@
+﻿//Singlr-Level Inheritance
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class employee
+    {
+       public int salary = 13000;
+    }
+    public class programmer:employee
+    {
+        public float bonous = 5000;
+    }
+    internal class InheritanceTest
+    {
+        public static void Main()
+        {
+            programmer p1=new programmer();
+            Console.WriteLine("Salary=" + p1.salary);
+            Console.WriteLine("Bonus=" + p1.bonous);
+
+        }
+    }
+}*/
+//In the above example, Employee is the base class and Programmer is the derived class.
