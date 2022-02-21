45e898ba2c0efe02dc8e8f3962d156c24d80d39a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:24:23 +0530

    Add files via upload

---
+++ Class149.cs
@@ -0,0 +1,28 @@
+﻿//4) C# Protected Internal Access Specifier
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class InternalTest
+    {
+        protected internal string name = "Paritodh Thakre";
+
+        protected internal void Mag(string msg)
+        {
+            Console.WriteLine("Hello=" + msg);
+        }
+    }
+    internal class Class149
+    {
+       static void Main()
+        {
+            InternalTest InternalTest = new InternalTest();
+            Console.WriteLine("Name="+InternalTest .name);  
+            InternalTest.Msg("Aniket");
+        }
+    }
+}*/
