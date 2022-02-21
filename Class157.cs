d26c8466d1b983cf4c1265648ae3e4e747e10694
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:26:33 +0530

    Add files via upload

---
+++ Class157.cs
@@ -0,0 +1,56 @@
+﻿//assignment : repeat the above program for person and employee class
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class person
+    {
+        string name;
+        int age;
+        public void getpersondata(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void getdisplaypersondata()
+        {
+            Console.WriteLine("Name="+this.name);
+            Console.WriteLine("Age="+this.age);    
+        }
+    }
+    
+    public class employee:person
+    {
+        public int empno;
+        public int salary;
+        public string designation;
+        public void getstudetndata(int empno,int salary,string designation)
+        {
+            this.empno = empno;
+            this.salary = salary;   
+            this.designation = designation;
+        }
+        public void getdisplaystudentdata()
+        {
+            Console.WriteLine("Empno="+this.empno); 
+            Console.WriteLine("Salary="+this.salary);
+            Console.WriteLine("Designation=" + this.designation);
+        }
+    }
+    internal class Class157
+    {
+        public static void Main()
+        {
+            employee e1=new employee();
+            e1.getpersondata("Mayuri" ,24);
+            e1.getdisplaypersondata ();
+            e1.getstudetndata(1, 25000, ".NET Developer");
+            e1.getdisplaystudentdata(); 
+
+        }
+    }
+}*/
