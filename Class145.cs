2bd605f52d12530eede7de5991903e3560e7b6b5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:22:43 +0530

    Add files via upload

---
+++ Class145.cs
@@ -0,0 +1,30 @@
+﻿//1) C# Public Access Specifier
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class publictest
+    {
+        public string name = "Sharukh Khan";
+        public void Msg(string msg)
+        {
+            Console.WriteLine("Hello" + msg);
+        }
+    }
+    internal class Class145
+    {
+        static void Main()
+        {
+            publictest p1=new publictest();
+            // Accessing public variable  
+            Console.WriteLine("Name=" + p1.name);
+            // Accessing public function  
+            p1.Msg("Ketan");
+
+        }
+    }
+}*/
