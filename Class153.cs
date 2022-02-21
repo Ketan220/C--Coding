7621e3c668b3f05a371f5d2c7aac576cf885b56a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:25:22 +0530

    Add files via upload

---
+++ Class153.cs
@@ -0,0 +1,36 @@
+﻿//another example 
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class person
+    {
+        public string name;
+        public string address;
+    }
+    public class student:person
+    {
+        public int rno;
+        public int marks;
+    }
+    internal class Class153
+    {
+        public static void Main()
+        {
+            student p2=new student();//creating object of child class
+            p2.name = "Aniket";
+            p2.address = "Nandanwan";
+            p2.rno = 12;
+            p2.marks = 250;
+
+            Console.WriteLine("Name=" + p2.name);
+            Console.WriteLine("Address=" + p2.address);
+            Console.WriteLine("Rno=" + p2.rno);
+            Console.WriteLine("Marks=" + p2.marks);
+        }
+    }
+}*/
