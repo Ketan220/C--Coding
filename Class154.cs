e890e4e4bf6914e9a9a7330b150f8a0c76b34798
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:25:57 +0530

    Add files via upload

---
+++ Class154.cs
@@ -0,0 +1,35 @@
+﻿/*assignment : create a base class person having fields name and address,
+create a derieved class employee having fields empno,salary and designation.*/
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class person
+    {
+        public string name = "Prathmesh";
+        public string address = "Dighori";
+    }
+    public class employee:person 
+    {
+        public int empno = 1;
+        public float salary = 10000;
+        public string disgination = ".Net Developer";
+    }
+    internal class Class154
+    {
+        public static void Main()
+        {
+            employee p3 = new employee();
+            Console.WriteLine("Name=" + p3.name);
+            Console.WriteLine("Address="+ p3.address);
+            Console.WriteLine("Empno=" + p3.empno);
+            Console.WriteLine("Salary=" + p3.salary);
+            Console.WriteLine("Dsignation="+p3.disgination);
+
+        }
+    }
+}*/
