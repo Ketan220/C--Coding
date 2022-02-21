bdac09f28bd607aca8d4ff534fd930055117da7b
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:24:54 +0530

    Add files via upload

---
+++ Class151.cs
@@ -0,0 +1,27 @@
+﻿//5) C# Private Access Specifier
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    
+    internal class program
+    {
+        private string name = "Amit Shah";
+
+        private void Msg(string msg)
+        {
+            Console.WriteLine("Hello=" + msg);
+        }
+        static void Main()
+        {
+            program privatetest = new program();
+            Console.WriteLine("Name=" + privatetest.name);
+            privatetest.Msg("Varsha");
+        }
+
+    }
+}*/
