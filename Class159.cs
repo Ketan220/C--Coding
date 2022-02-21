bb0dad0fca3fe744cf28bafe63786bd4e8b15af5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-21 21:27:28 +0530

    Add files via upload

---
+++ Class159.cs
@@ -0,0 +1,55 @@
+﻿//example of protected internal access modifier
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class person
+    {
+        string name;
+        string address;
+
+        protected internal void getpersondata(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        protected internal void getdisplaypersondata()
+        {
+            Console.WriteLine("Name=" + this.name);
+            Console.WriteLine("Address=" + this.address);
+        }
+    }
+    public class student : person
+    {
+        int rno;
+        int marks;
+        public void getstudentdata(int rno, int marks)
+        {
+            //getpersondata("kb", "Mahal");//calling base class method from derieved class
+            this.rno = rno;
+            this.marks = marks;
+        }
+        public void getdisplaystudentdata()
+        {
+            //getdisplaypersondata();//calling base class method from derieved class
+            Console.WriteLine("Rno=" + this.rno);
+            Console.WriteLine("Marks=" + this.marks);
+        }
+    }
+    internal class testinheritance
+    {
+        public static void Main()
+        {
+            student s1 = new student();
+            s1.getpersondata("kb", "Mahal"); //since testinhertance class in not derived class of person
+            s1.getdisplaypersondata();       //since testinhertance class in not derived class of person
+            s1.getstudentdata(1, 40);
+            s1.getdisplaystudentdata();
+        }
+
+    }
+}*/
