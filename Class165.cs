0de1054170e25805cc0de69c50d7ae1dd9d73bc1
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-22 19:39:36 +0530

    Add files via upload

---
+++ Class165.cs
@@ -0,0 +1,33 @@
+﻿//C# base keyword example: calling base class method
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating.........");
+        }
+    }
+    public class Dog:Animal
+    {
+       public void eat()
+        {
+            base.eat();
+            Console.WriteLine("Barking........");
+        }
+    }
+    internal class Class165
+    {
+        public static void Main()
+        {
+            Dog d2=new Dog();   
+            d2.eat();   
+        }
+    }
+}*/
