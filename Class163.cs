b1ef3c941354333f75a92048f90e64b25f3356dc
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-22 19:38:58 +0530

    Add files via upload

---
+++ Class163.cs
@@ -0,0 +1,52 @@
+﻿/*Let's see an example of aggregation where Employee class has the reference of Address class as data member. 
+ * In such way, it can reuse the members of Address class.*/
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Address
+    {
+        public string addressline,city ,state;  
+        public  Address(string addressline,string city,string state)
+        {
+            this.addressline = addressline;
+            this.city = city;
+            this.state = state;
+        }
+    }
+    public class Employee
+    {
+        public int id;
+        public string name;
+        public Address address;
+
+        public  Employee(int id,string name,Address address)
+        {
+            this.id = id;
+            this.name = name;   
+            this.address = address;
+        }
+        public void display()
+        {
+            Console.WriteLine("Id=" + id);
+            Console.WriteLine("Name="+name);
+            Console.WriteLine("AddressLine="+address.addressline);
+            Console.WriteLine("City=" + address.city);
+            Console.WriteLine("State="+address.state);
+
+        }
+    }
+    internal class Class163
+    {
+        public static void Main()
+        {
+            Address a1 = new Address("Noida", "Delhi", "Maharashtra");
+            Employee e1 = new Employee(1, "krunal", a1);
+            e1.display();   
+        }
+    }
+}*/
