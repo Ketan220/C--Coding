d2e00cd30d38c0219504e1ed103c7deb56792252
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-22 19:39:17 +0530

    Add files via upload

---
+++ Class164.cs
@@ -0,0 +1,32 @@
+﻿/*Let's see the simple example of base keyword in C# which accesses the field of base class.*/
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class animal
+    {
+        public string color="red";  
+    }
+    public class dog:animal
+    {
+        public string color = "brown";
+        
+        public void showcolor()
+        {
+            Console.WriteLine("Color="+base.color);
+            Console.WriteLine("Color="+color);
+        }
+    }
+    internal class Class164
+    {
+       public static void Main()
+        {
+            dog d1 = new dog();
+            d1.showcolor();
+        }
+    }
+}*/
