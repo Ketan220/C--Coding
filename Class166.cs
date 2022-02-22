ea128754b14c8c4297c37558b4dfb5f9a46677d7
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-22 19:40:00 +0530

    Add files via upload

---
+++ Class166.cs
@@ -0,0 +1,31 @@
+﻿//C# inheritance: calling base class constructor internally
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animale.......");
+        }
+    }
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine ("Dog........");   
+        }
+    }
+    internal class Class166
+    {
+        public static void Main()
+        {
+            Dog d1=new Dog();   
+        }
+    }
+}*/
