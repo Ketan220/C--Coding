001b23f29f04afd641682783c725da8f8de8dc3a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-23 20:10:38 +0530

    Add files via upload

---
+++ Class168.cs
@@ -0,0 +1,32 @@
+﻿//Let 's see a simple example of runtime polymorphism in C#.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating........");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating bread.........");
+        }
+    }
+    internal class TestOveririding
+    {
+        public static void Main()
+        {
+            Animal d = new Dog();
+            d.eat();
+        }
+    }
+}*/
