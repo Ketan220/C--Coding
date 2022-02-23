db68bdbfdedd67ed6ef8e3fa7269c989d1116c69
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-23 20:14:17 +0530

    Add files via upload

---
+++ Class172.cs
@@ -0,0 +1,53 @@
+﻿//repeat the above program by accepting values from the user
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Account
+    {
+        public int actno;
+        public int balance = 10000;
+
+        public virtual string deposit(int actno, int amount)
+        {
+            return "deposite method of account class";
+        }
+        public string showbalance()
+        {
+            return "Balance=" + balance;
+
+        }
+    }
+    public class Current:Account
+    {
+        public override string deposit(int actno, int amount)
+        {
+            this.actno = actno;
+            balance = balance + amount;
+            return "Amount deposited successfully in current class";
+        }
+
+    }
+    internal class Class172
+    {
+        public static void Main()
+        {
+            Account act=new Current();
+            Console.WriteLine("Enter acount no.");
+            int actno=Convert.ToInt32(Console.ReadLine ());
+            Console.WriteLine("Enter amount");
+            int amount=Convert.ToInt32(Console.ReadLine());
+
+            string res = act.deposit(actno, amount);
+            Console.WriteLine("Account no is=" + act.actno);
+            Console.WriteLine(res); 
+            string res2 = act.showbalance();
+            Console.WriteLine(res2);
+         
+        }
+    }
+}*/
