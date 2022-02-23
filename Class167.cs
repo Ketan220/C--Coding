abdb6a5106eacd23fdca746fa0512b2b6eb6086f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-23 20:10:09 +0530

    Add files via upload

---
+++ Class167.cs
@@ -0,0 +1,33 @@
+﻿/*Let's see a simple example of method overriding in C#. In this example, we are overriding the eat() method by the help of override
+keyword. */
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating........");
+        }
+    }
+    class Dog:Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating bread.........");
+        }
+    }
+    internal class TestOveririding
+    {
+        public static void Main()
+        {
+            Animal d = new Dog();
+            d.eat();
+        }
+    }
+}*/
