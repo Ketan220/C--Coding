2096e9946199d6e0fcf032f8c4a64154d2495536
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-23 20:13:43 +0530

    Add files via upload

---
+++ Class170.cs
@@ -0,0 +1,30 @@
+﻿/*Runtime Polymorphism with Data Members
+
+Runtime Polymorphism can't be achieved by data members in C#. Let's see an example where we are accessing the field by 
+reference variable which refers to the instance of derived class.*/
+
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public  class Animal
+    {
+        public string color = "White";
+    }
+    public class Dog:Animal
+    {
+        public string color = "Black";
+    }
+    internal class Class170
+    {
+        public static void Main()
+        {
+            Animal d = new Dog();
+            Console.WriteLine(d.color); 
+        }
+    }
+}*/
