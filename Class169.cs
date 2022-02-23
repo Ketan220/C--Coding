efb52d34a3e3fd786221362afff1c9496bbefd2e
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-23 20:10:53 +0530

    Add files via upload

---
+++ Class169.cs
@@ -0,0 +1,44 @@
+﻿//Let 's see a another example of runtime polymorphism in C# where we are having two derived classes.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class shape
+    {
+        public virtual void draw()
+        {
+            Console.WriteLine("Drawing shape..........");
+        }
+    }
+    public class rectangle: shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("draing Rectangle..........");
+        }
+    }
+    public class circle:shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Circle.............");
+        }
+    }
+    internal class Class169
+    {
+       public static void Main()
+        {
+            shape s = new rectangle();
+            s.draw();
+
+            s = new circle();
+            s.draw();
+
+        }
+
+    }
+}*/
