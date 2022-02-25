241fd663076c842f4437c39d0ce4630ce73e2a3b
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-25 10:37:20 +0530

    Add files via upload

---
+++ Class176.cs
@@ -0,0 +1,39 @@
+﻿//Example Abstraction
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    abstract class person
+    {
+        //abstract method 
+        public abstract void display();
+
+        //non-abstract method 
+        public void showdata()
+        {
+            Console.WriteLine("Hello non -abstract showdata method ");
+        }
+    }
+    class student:person
+    {
+        public override void display()
+        {
+            //implementation of abstract method display in derieved class
+            Console.WriteLine("Hello abstarct display method");
+        }
+    }
+    internal class Class176
+    {
+      public static void Main()
+        {
+            //person p = new person(); error since person is abstract class
+            student s = new student();
+            s.display();
+            s.showdata();
+        }
+    }
+}*/
