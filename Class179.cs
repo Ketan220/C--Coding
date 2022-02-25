5d72a7f9a97ad0ebdda75350fab06316e966d2cb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-25 10:37:54 +0530

    Add files via upload

---
+++ Class179.cs
@@ -0,0 +1,41 @@
+﻿/*Let's see the example of interface in C# which has draw() method. Its implementation is provided by two classes: Rectangle and Circle.*/
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public interface drawable
+    {
+        void draw();
+    }
+    public class Reactangle:drawable
+    {
+        public  void draw()
+        {
+            Console.WriteLine("Drawing Reactangle...............");
+        }
+    }
+    public class Circle:drawable
+    {
+        public void  draw()
+        {
+            Console.WriteLine("Drawing  Circle................");
+        }
+    }
+    internal class Class179
+    {
+        public static void Main()
+        {
+            drawable d;
+            d = new Reactangle();
+            d.draw();   
+
+            d=new Circle();
+            d.draw();
+        }
+
+    }
+}*/
