8ceb4a4be323808e81633c21e69b7274cc914f63
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-25 10:38:59 +0530

    Add files via upload

---
+++ Class183.cs
@@ -0,0 +1,36 @@
+﻿//C# Encapsulation
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Student
+    {
+        // Creating setter and getter for each property  
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    internal class Class183
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            // Setting values to the properties  
+            student.ID = "101";
+            student.Name = "Mohan Ram";
+            student.Email = "mohan@example.com";
+
+
+            // getting values  
+            Console.WriteLine("ID = " + student.ID);
+            Console.WriteLine("Name = " + student.Name);
+            Console.WriteLine("Email = " + student.Email);
+        }
+
+    }
+}*/
