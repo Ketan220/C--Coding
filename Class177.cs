21678d9692a16385692dd4030f3b966dbb75ee7f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-25 10:37:32 +0530

    Add files via upload

---
+++ Class177.cs
@@ -0,0 +1,40 @@
+﻿/*Let's see an example of abstract class in C# which has one abstract method draw(). Its implementation is provided by derived classes: 
+ * Rectangle and Circle. Both classes have different implementation.*/
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+     abstract class shape
+    {
+        public abstract void draw();
+    }
+     class Reactangle:shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Rectangle........");
+        }
+    }
+     class Circle:shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing Circle........");
+        }
+    }
+    internal class Class177
+    {
+        public static void Main()
+        {
+            shape s;
+            s=new Reactangle();
+            s.draw();
+            s=new Circle();
+            s.draw();
+        }
+    }
+}*/
