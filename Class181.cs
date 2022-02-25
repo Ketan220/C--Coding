306e4caa40ba78b81ae7ec0801a4cc32c3901d57
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-25 10:38:35 +0530

    Add files via upload

---
+++ Class181.cs
@@ -0,0 +1,40 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    interface bank
+    {
+        string deposit(int actno, int amount);
+    }
+    class account
+    {
+        public int actno;
+        public int balance=1000;
+    }
+    class saving:account,bank
+    {
+        public string deposit(int actno,int amount)
+        {
+            this.actno = actno;
+            balance = balance + amount;
+            return "Amount succefully deposit in saving class,Balance is=" + balance; 
+
+        }
+    }
+    internal class Class181
+    {
+        public static void Main()
+        {
+            bank b;
+
+            b= new saving();
+
+            string res = b.deposit(121, 2000);
+            Console.WriteLine(res);
+        }
+    }
+}*/
