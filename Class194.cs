d77b9cbb1dd6d6c47d252994e584eb2c08ebd757
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-27 21:04:46 +0530

    Add files via upload

---
+++ Class194.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class194
+    {
+        struct emp
+        {
+            public string name;
+            public int age;
+            public int bs;
+
+        };
+        static void Main()
+        {
+            emp e;
+            Console.WriteLine("Enter namge,age and bs");
+            e.name =Console.ReadLine();     
+            e.age =Convert.ToInt32(Console.ReadLine()); 
+            e.bs =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Name=" + e.name);
+            Console.WriteLine("Age=" + e.bs);
+            Console.WriteLine("Bs=" + e.bs);
+        }
+    }
+}
