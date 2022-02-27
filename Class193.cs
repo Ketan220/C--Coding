622eebcdd07135092bad85f24a05f5550b4ff916
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-27 21:04:30 +0530

    Add files via upload

---
+++ Class193.cs
@@ -0,0 +1,23 @@
+﻿/* using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class193
+    {
+        static void Main()
+        {
+            string str;
+            
+            Console.WriteLine("Enter any word");
+            str=Console .ReadLine();
+            int strLength = str.Length;
+            Console.WriteLine("Word=" + str);
+            Console.WriteLine("WordLength="+strLength);
+
+        }
+    }
+}*/
