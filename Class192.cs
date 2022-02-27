15f99fa14075e4e6973ddf0b85ea3a95eca36776
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-27 21:04:16 +0530

    Add files via upload

---
+++ Class192.cs
@@ -0,0 +1,54 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class192
+    {
+        static void Main()
+        {
+            int i, j, flag = 0;
+            int[,] a = new int[4, 4];
+            int[,] b = new int[4, 4];
+            for(i=0; i<=3; i++)
+            {
+                for(j=0; j<=3; j++)
+                {
+                    Console.WriteLine("Enter the element");
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            } 
+            for(i=0; i<=3; i++)
+            {
+                for(j=0; j<=3; j++)
+                {
+                    b[i, j] = a[i, j];
+                }
+            }
+            for(i=0;i<=3; i++)
+            {
+                for(j=0;j<=3;j++)
+                {
+                    if(a[i,j]!=b[i,j])
+                    {
+                        flag = 1;
+                        break;
+
+                    }
+                }
+            }
+            if(flag==1)
+            {
+                Console.WriteLine("Matrix is Symmetrical");
+            }
+            else
+            {
+                Console.WriteLine("Matrix is not seymmetrical");
+            }
+
+        }
+    }
+}*/
