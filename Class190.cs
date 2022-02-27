842c26563f5d5b1be48e21d4d04918360e287e81
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-27 21:04:01 +0530

    Add files via upload

---
+++ Class190.cs
@@ -0,0 +1,43 @@
+﻿//Scalar Multiplication
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class190
+    {
+        public static void Main()
+        {
+            int i, j, num;
+            int[,] arr = new int[4, 4];
+            for(i=0;i<=3;i++)
+            {
+                for(j=0;j<=3;j++)
+                {
+                    Console.WriteLine("Enter the element");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Enter the number");
+            num=Convert.ToInt32(Console.ReadLine());
+            for(i=0;i<=3;i++)
+            {
+                for(j=0;j<=3;j++)
+                {
+                    arr[i, j] = arr[i, j] * num;
+                }
+            }
+            for(i=0;i<=3;i++)
+            {
+                for(j=0;j<=3;j++)
+                {
+                    Console.Write("\t"+arr[i, j]);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}*/
