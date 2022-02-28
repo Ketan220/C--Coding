6fe3ac75fb4f69989e8d701f801ae55a50dfb3f3
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:22:53 +0530

    Add files via upload

---
+++ Class207.cs
@@ -0,0 +1,32 @@
+﻿//example of sort method
+/*using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    internal class Class207
+    {
+        static void Main()
+        {
+            ArrayList ar=new ArrayList();   
+            ar.Add(19);
+            ar.Add(12);
+            ar.Add(13);
+            ar.Add(16);
+            ar.Add(9);
+
+            Console.WriteLine("ArrayList collection without sorting");
+            foreach(int obj in ar)
+            {
+                Console.WriteLine("Student rno="+obj);
+            }
+
+            ar.Sort();
+            Console.WriteLine("ArrayList Collection with sorting");
+            foreach(int obj in ar)
+            {
+                Console.WriteLine("Student rno="+obj);
+            }
+        }
+    }
+}*/
