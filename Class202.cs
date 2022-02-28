9b6aa97ffacca628e6da9425730cb7badca62922
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:20:06 +0530

    Add files via upload

---
+++ Class202.cs
@@ -0,0 +1,17 @@
+﻿//Let's see a simple example of namespace which contains one class "Program".
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello namespace");
+        }
+    }
+}*/
