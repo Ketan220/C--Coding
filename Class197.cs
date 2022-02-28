a68fc01572be84a2270f64731be1655df14ec655
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:08:25 +0530

    Add files via upload

---
+++ Class197.cs
@@ -0,0 +1,41 @@
+﻿//C# static field example
+//Let's see the simple example of static field in C#.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Account
+    {
+        public int actno;
+        public string name;
+        public static float rateofinterest = 7.5f;
+
+        public Account(int actno,string name)
+        {
+            this.actno = actno;
+            this.name = name;
+
+        }
+        public void display()
+        {
+            Console.WriteLine(actno + " " + name + " " + rateofinterest);
+        }
+        
+        
+
+    }
+    internal class Class197
+    {
+        public static void Main()
+        {
+            Account a1 = new Account(12, "Saboo");
+            Account a2 = new Account(16, "Vikrant");
+            a1.display();
+            a2.display();   
+        }
+    }
+}*/
