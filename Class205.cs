eea583b8f46494acc3aaeac658abd34bdcbcedcc
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:22:20 +0530

    Add files via upload

---
+++ Class205.cs
@@ -0,0 +1,22 @@
+﻿//Example of array list
+/*using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    internal class Class205
+    {
+        static void Main()
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add("Amit");
+            ar.Add("Ramu");
+            ar.Add("Shayam");
+            //displaying values of arraylist
+            foreach (object obj in ar)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}*/
