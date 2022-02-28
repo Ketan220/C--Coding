988fc4a9ce626a713a33f8099ad6ee9d82e7c1ee
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:23:20 +0530

    Add files via upload

---
+++ Class209.cs
@@ -0,0 +1,40 @@
+﻿//example of remove method
+/*using System;
+using System.Collections;
+
+
+namespace ConsoleApp2
+{
+    internal class Class209
+    {
+        static void Main()
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(19);
+            ar.Add(12);
+            ar.Add(13);
+
+            Console.WriteLine("ArrayList collection withoout Sorting");
+            foreach(int obj in ar)
+            {
+                Console.WriteLine("Student rno="+obj);
+            }
+
+            ar.Remove(13);
+            Console.WriteLine("array list colection after removing 13");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("Student rno=" + obj);
+            }
+
+            ar.RemoveAt(1);
+            Console.WriteLine("array list colection after removing at 1");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("Student rno=" + obj);
+            }
+
+
+        }
+    }
+}*/
