95f8c0be8c2bffee2306bc57bff40e555a9d50a7
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:22:37 +0530

    Add files via upload

---
+++ Class206.cs
@@ -0,0 +1,24 @@
+﻿/*using System;
+using System.Collections;
+
+
+namespace ConsoleApp2
+{
+    internal class Class206
+    {
+        static void Main()
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(1); 
+            ar.Add(2); 
+            ar.Add(3);
+            
+            foreach(int obj in ar)
+            {
+                Console.WriteLine("Student rno="+obj);
+            }
+            Console.WriteLine("Capacity={0}",ar.Capacity);  
+            Console.WriteLine("Count={0}",ar.Count);     
+        }
+    }
+}*/
