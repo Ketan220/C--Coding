86a643f468a83e2d17b04ad41b4e2da15739d3f8
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:20:20 +0530

    Add files via upload

---
+++ Class203.cs
@@ -0,0 +1,41 @@
+﻿//C# namespace example: by fully qualified name
+//Let's see another example of namespace in C# where one namespace program accesses another namespace program.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    public class hello
+    {
+        public void sayhello()
+        {
+            Console.WriteLine("Hello first  namespace");
+        }
+    }
+}
+namespace second
+{
+    public class hello
+    {
+        public void sayhello()
+        {
+            Console.WriteLine("Hello second namespace");
+        }
+    }
+}
+    internal class Class203
+    {
+    public static void Main()
+    {
+        first.hello h1=new first.hello();   
+        second.hello h2=new second.hello();
+
+        h1.sayhello();
+        h2.sayhello();  
+    }
+
+    }*/
+
