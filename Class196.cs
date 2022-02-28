db78174fea24474e93232a99cb9372082382553d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:08:08 +0530

    Add files via upload

---
+++ Class196.cs
@@ -0,0 +1,22 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void display()
+        {
+            Console.WriteLine("hello");
+        }
+        static void Main()
+        {
+
+            display(); //to access static method display instance is not required
+        }
+    }
+
+}*/
