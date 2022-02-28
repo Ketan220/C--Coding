542170bb8e54c084df6dbd47bdff9e24cb79c6c2
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:03:54 +0530

    Add files via upload

---
+++ Class195.cs
@@ -0,0 +1,24 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class program
+    {
+        void display()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main()
+        {
+            program p=new program();    
+            p.display();    
+        }
+    }
+}*/
+//so p is an instance of class program and to access non static display method
+//we access it through p instance
+
