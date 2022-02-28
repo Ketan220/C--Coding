53181f446b43bd0dd93fb694432189dd298131d8
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:23:06 +0530

    Add files via upload

---
+++ Class208.cs
@@ -0,0 +1,31 @@
+﻿//example of insert method
+/*using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    internal class Class1208cs
+    {
+        static void Main()
+        {
+            ArrayList ar=new ArrayList();
+            ar.Add(19);
+            ar.Add(12);
+            ar.Add(13);
+
+            Console.WriteLine("ArrayList collection without Sorting ");
+            foreach(int obj in ar)
+            {
+                Console.WriteLine("Student rno="+obj);
+            }
+
+
+            ar.Insert(2, 17);
+            Console.WriteLine("ArrayList collection after adding element at 2 position");
+            foreach(int obj in ar)
+            {
+                Console.WriteLine("Student rno="+obj);
+            }
+        }
+    }
+}*/
