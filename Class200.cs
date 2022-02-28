749e29d50e5aeb72576f88ac49159d7d405202f0
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:19:31 +0530

    Add files via upload

---
+++ Class200.cs
@@ -0,0 +1,28 @@
+﻿//C# static class example
+//Let's see the example of static class that contains static field and static method.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public static class math
+    {
+        public static float pi = 3.14f;
+        public static int cube(int n)
+        {
+            return (n * n * n);
+        }
+
+    }
+    internal class Class200
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Value of PI="+math.pi);  
+            Console.WriteLine("Cube of 3="+math.cube(3));
+        }
+    }
+}*/
