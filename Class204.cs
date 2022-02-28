6a8219d91035766c964d0c230aaa92864d6c1b87
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:20:34 +0530

    Add files via upload

---
+++ Class204.cs
@@ -0,0 +1,38 @@
+﻿//C# namespace example: by using keyword
+//Let's see another example of namespace where we are using "using" keyword so that we don't have to use complete name for accessing a namespace program.
+/*using System;
+using first;
+using second;
+
+namespace first
+{
+    public class hello
+    {
+        public void sayhello()
+        {
+            Console.WriteLine("Hello first namespace");
+        }
+    }
+}
+namespace second
+{
+    public class welcome
+    {
+        public void saywelcome()
+        {
+            Console.WriteLine("Saywelcome second namespace");
+        }
+    }
+}
+    internal class Class204
+    {
+        public static void Main()
+        {
+              hello h = new hello();    
+              welcome w=new welcome();
+
+              h.sayhello();
+              w.saywelcome();
+        }
+    }*/
+
