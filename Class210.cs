b6c14b0e6146dce539c27b16ca9a8b196eb059cb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-28 22:23:47 +0530

    Add files via upload

---
+++ Class210.cs
@@ -0,0 +1,39 @@
+﻿//create  a student class and create a arraylist collection of objects of student class
+using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    class student
+    {
+        public int rno;
+        public string name;
+
+        public student(int rno,string name)
+        {
+            this.rno = rno; 
+            this.name = name;
+        }
+
+    }
+    internal class Class210
+    {
+        static void Main()
+        {
+            student s1 = new student(1,"Mahesh");
+            student s2 = new student(3,"Yamini");
+            student s3 = new student(5,"Sam");
+
+            ArrayList al=new ArrayList();
+            al.Add(s1);
+            al.Add(s2); 
+            al.Add(s3);
+
+            foreach(student s in al)
+            {
+                Console.WriteLine("Rno="+s.rno);
+                Console.WriteLine("Name="+s.name);
+            }
+        }
+    }
+}
