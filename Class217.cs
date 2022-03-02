eb14049dd7909da06caee09101dd2c56212e1455
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-02 21:41:33 +0530

    Add files via upload

---
+++ Class217.cs
@@ -0,0 +1,35 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class217
+    {
+        static void Main()
+        {
+            int n, i, r;
+            Console.WriteLine("Enter number");
+            n=Convert.ToInt32(Console.ReadLine());
+            for(i=0;i<n-1;i++)
+            {
+                r = rfebo(i);
+                Console.Write("\t"+r);
+            }
+        }
+        static int rfebo(int pos)
+        {
+            if(pos==0 || pos==1)
+            {
+                return (pos);
+            }
+            else
+            {
+                return(rfebo(pos-1)+rfebo(pos-2));
+            }
+
+        }
+    }
+}*/
