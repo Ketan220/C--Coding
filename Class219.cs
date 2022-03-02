58b86315072828750b932545c27e9c2dc68e72f9
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-02 21:42:05 +0530

    Add files via upload

---
+++ Class219.cs
@@ -0,0 +1,24 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class219
+    {
+        static void Main()
+        {
+            int n, i=0, term = 0;
+            Console.WriteLine("Enter no. of terms required");
+            n=Convert.ToInt32(Console.ReadLine());
+            while(i<=n)
+            {
+                Console.Write("\t"+term);
+                term = term * 2 + 1;
+                i++;
+            }
+        }
+    }
+}*/
