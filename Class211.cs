27cc6574b088a252f177fb4b26fd05750b3f2afb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-02 21:40:57 +0530

    Add files via upload

---
+++ Class211.cs
@@ -0,0 +1,32 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            int num, r;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            r = rfact(num);
+            Console.WriteLine("Factorial=" + r);
+        }
+         static int rfact(int n)
+        {
+            if(n==0)
+            {
+                return (1);
+            }
+            else
+            {
+                return (n * rfact(n - 1));
+            }
+        }
+    }
+}*/
+
