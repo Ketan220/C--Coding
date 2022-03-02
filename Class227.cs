ef7662801bafaf40139e9c7bc7ddad0b49e5e6bd
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-02 21:43:12 +0530

    Add files via upload

---
+++ Class227.cs
@@ -0,0 +1,24 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class227
+    {
+        static void Main()
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}*/
