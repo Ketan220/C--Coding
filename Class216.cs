0a95110d2000ec2632f977dfcc25fae74df06136
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-02 21:41:15 +0530

    Add files via upload

---
+++ Class216.cs
@@ -0,0 +1,31 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class216
+    {
+        static void Main()
+        {
+            int num, r;
+            Console.WriteLine("Enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            r = rsumd(num);
+            Console.WriteLine("Sum of digit=" + r);
+        }
+        static int rsumd(int n)
+        {
+            if(n==0)
+            {
+                return (0);
+            }
+            else
+            {
+                return (n%10+rsumd(n/10));
+            }
+        }
+    }
+}*/
