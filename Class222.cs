5dfaf0d33cb6f253bce1da1e610f5c91248617cc
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-02 21:42:49 +0530

    Add files via upload

---
+++ Class222.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*namespace ConsoleApp2
+{
+    internal class Class222
+    {
+       static void Main()
+        {
+            float term, sum = 0,s;
+            int nr = 1, dr = 1, i = 1, x, n;
+            Console.WriteLine("Enter the value of x and n");
+            x=Convert.ToInt32(Console.ReadLine());
+            n=Convert.ToInt32(Console.ReadLine());  
+            while(i<=n)
+            {
+                term = nr/(dr * 1.0f);
+                sum = sum + term;
+                nr = nr * x;
+                dr = dr + 1;
+                i++;
+            }
+            Console.WriteLine("S=" + sum);
+        }
+
+    }
+}*/
