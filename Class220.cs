d540052805e3ac9355d3432708c75d616b5baac5
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-02 21:42:21 +0530

    Add files via upload

---
+++ Class220.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class220
+    {
+        static void Main()
+        {
+            int n, term = 0,diff = 1,i= 0;
+            Console.WriteLine("Enter number");
+            n=Convert.ToInt32(Console.ReadLine());  
+            while(i<=n)
+            {
+                Console.Write("\t" + term);
+                term = term + diff;
+                diff = diff * 2;
+                i++;    
+            }
+
+        }
+    }
+}*/
