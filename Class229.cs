6c18f253e8e15120eda513ad23d51a87ae024fc1
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:40:16 +0530

    Add files via upload

--- Class229.cs
+++ Class229.cs
@@ -1,8 +1,6 @@
-﻿/*using System;
+﻿//DictionaryList class of generic collection 
+/*using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
@@ -10,16 +8,45 @@ namespace ConsoleApp2
     {
         static void Main()
         {
-            int i, j;
-            for(i=5;i>=1;--i)
+
+            Dictionary<string, string> domains = new Dictionary<string, string>();
+
+            domains.Add("de", "Germany");
+            domains.Add("sk", "Slovakia");
+            domains.Add("us", "United States");
+            domains.Add("ru", "Russia");
+            domains.Add("hu", "Hungary");
+            domains.Add("pl", "Poland");
+
+            Console.WriteLine(domains["sk"]);
+            Console.WriteLine(domains["ru"]);
+
+            Console.WriteLine("Dictionary has {0} items",domains.Count);
+
+
+            Console.WriteLine("Keys of the dictionary:");
+            List<string> keys = new List<string>(domains.Keys);
+            foreach (string key in keys)
             {
-                for(j=1;j<=i;j++)
-                {
-                    Console.Write(j);
-                }
-                Console.Write("\n");    
+                Console.WriteLine("Keys:{0}", key);
             }
-     
+
+
+            Console.WriteLine("Values of the dictionary:");
+            List<string> vals = new List<string>(domains.Values);
+            foreach (string val in vals)
+            {
+                Console.WriteLine("{0}", val);
+            }
+
+
+            Console.WriteLine("Keys and values of the dictionary:");
+            foreach (KeyValuePair<string, string> kvp in domains)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",kvp.Key, kvp.Value);
+            }
+
+
         }
     }
 }*/
