d815884cc5ec126a799fd9683db37dbfdb78f9c6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:39:28 +0530

    Add files via upload

---
+++ Class226.cs
@@ -0,0 +1,58 @@
+﻿//another example of quee class
+/*using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    internal class Class226
+    {
+        static void Main()
+        {
+            Queue q = new Queue();
+
+            q.Enqueue("Anand");
+            q.Enqueue("Maria");
+            q.Enqueue("Gomes");
+            q.Enqueue("William");
+
+            Console.WriteLine("Current queue: ");
+            foreach (String c in q)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+            q.Enqueue("Vinita");
+            q.Enqueue("Harshal");
+            Console.WriteLine("Current queue: ");
+            foreach (String c in q)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Removing some values ");
+            String ch = (String)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+            Console.WriteLine("Current queue: ");
+            foreach (String c in q)
+            {
+                Console.Write(c + " ");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Removing some values ");
+            ch = (String)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+            Console.WriteLine("Current queue: ");
+            foreach (String c in q)
+            {
+                Console.Write(c + " ");
+            }
+
+
+
+        }
+    }
+}*/
