88a1654c194e3ee5dc6635570909dad269ad643f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:38:52 +0530

    Add files via upload

---
+++ Class225.cs
@@ -0,0 +1,56 @@
+﻿//Queue
+/*using System;
+using System.Collections;
+
+
+namespace ConsoleApp2
+{
+    internal class Class225
+    {
+        static void Main()
+        {
+            Queue q=new Queue();
+            q.Enqueue('A');
+            q.Enqueue('M');
+            q.Enqueue('G');
+            q.Enqueue('W');
+
+
+            Console.WriteLine("Current queue: ");
+            foreach (char c in q)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+            q.Enqueue('V');
+            q.Enqueue('H');
+
+            Console.WriteLine("Current queue: ");
+            foreach (char c in q)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+            char ch = (char)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+            foreach (char c in q)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+            ch = (char)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+            foreach (char c in q)
+            {
+                Console.Write(c + " ");
+            }
+
+
+
+
+        }
+    }
+}*/
