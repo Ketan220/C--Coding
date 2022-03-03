bf92fe54d19c1cae6179884f22cc37f403f31b57
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:41:54 +0530

    Add files via upload

---
+++ Class233.cs
@@ -0,0 +1,45 @@
+﻿//Stack
+/*using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    internal class Class233
+    {
+        static void Main()
+        {
+            Stack st=new Stack();
+            st.Push('A');
+            st.Push('M');
+            st.Push('G');
+            st.Push('W');
+            Console.WriteLine("Current Stack");
+            foreach(char c in st)
+            {
+                Console.Write(c+" ");
+            }
+            st.Push('V');
+            st.Push('H');
+            Console.WriteLine("The next popable value in the stack :{0}"+st.Peek());
+
+            Console.WriteLine("Current Stack");
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Removing values form last ");
+            st.Pop();
+            st.Pop();
+            st.Pop();
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }
+
+
+        }
+    }
+}*/
