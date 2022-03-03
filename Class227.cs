b7d89cbbc6c3e832b80a7b00ded6de0bbf253897
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:39:43 +0530

    Add files via upload

--- Class227.cs
+++ Class227.cs
@@ -1,8 +1,7 @@
-﻿/*using System;
+﻿//List class of Generic Collection
+/*using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp2
 {
@@ -10,15 +9,34 @@ namespace ConsoleApp2
     {
         static void Main()
         {
-            int i, j;
-            for (i = 1; i <= 5; i++)
+            List<string> langs = new List<string>();
+
+            langs.Add("Java");
+            langs.Add("C#");
+            langs.Add("C");
+            langs.Add("C++");
+            langs.Add("Ruby");
+            langs.Add("Javascript");
+
+            Console.WriteLine(langs.Contains("C#"));
+
+            Console.WriteLine("Value of index 1="+langs[1]);
+            Console.WriteLine("Value of index 2="+langs[2]);
+
+            langs.Remove("C#");
+            langs.Remove("C");
+            Console.WriteLine(langs.Contains("C#"));
+
+            langs.Insert(4, "Haskell");
+            langs.Sort();
+
+            foreach (string lang in langs)
             {
-                for (j = 1; j <= i; j++)
-                {
-                    Console.Write("* ");
-                }
-                Console.Write("\n");
+                Console.WriteLine(lang);
             }
+            
+            Console.WriteLine("Value of index 1="+langs[1]);
+            Console.WriteLine("Value of index 2="+langs[2]);
         }
     }
 }*/
