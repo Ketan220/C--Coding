cf7b64d08de2f216ab8d9490a30b6c475b8fcc74
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:42:15 +0530

    Add files via upload

---
+++ Class234.cs
@@ -0,0 +1,47 @@
+﻿//Queue class of generic collecton
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class234
+    {
+        static void Main()
+        {
+            Queue<string> msgs = new Queue<string>();
+
+            msgs.Enqueue("Message 1");
+            msgs.Enqueue("Message 2");
+            msgs.Enqueue("Message 3");
+            msgs.Enqueue("Message 4");
+            msgs.Enqueue("Message 5");
+
+            foreach (string msg in msgs)
+            {
+                Console.WriteLine(msg);
+            }
+
+            msgs.Enqueue("Message 6");
+            msgs.Enqueue("Message 7");
+            Console.WriteLine("The next popping value is:{0}", msgs.Peek());
+
+            foreach (string msg in msgs)
+            {
+                Console.WriteLine(msg);
+            }
+
+            Console.WriteLine("Removing first two");
+            msgs .Dequeue ();   
+            msgs.Dequeue ();
+            foreach (string msg in msgs)
+            {
+                Console.WriteLine(msg);
+            }
+
+
+        }
+    }
+}
