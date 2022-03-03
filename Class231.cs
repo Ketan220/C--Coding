ae5eedd2824b9e8959178a1e35e5e8f99bc80148
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:41:22 +0530

    Add files via upload

---
+++ Class231.cs
@@ -0,0 +1,41 @@
+﻿//HashTable
+/*using System;
+using System.Collections;
+
+
+namespace ConsoleApp2
+{
+    internal class Class231
+    {
+         static void Main()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("001", "Zara Ali");
+            ht.Add("002", "Abida Rehman");
+            ht.Add("003", "Joe Holzner");
+            ht.Add("004", "Mausam Benazir Nur");
+            ht.Add("005", "M. Amlan");
+            ht.Add("006", "M. Arif");
+            ht.Add("007", "Ritesh Saikia");
+
+            if(ht.Contains("Nahu Ali"))
+            {
+                Console.WriteLine("The student name alreafy in the list");
+            }
+            else
+            {
+                ht.Add("008", "Nahu Ali");
+            }
+
+            // Get a collection of the keys.
+            ICollection key = ht.Keys;
+
+            foreach(string k in key)
+            {
+                //Console.WriteLine("Key:"+k);
+                Console.WriteLine(k+":"+ht[k]);
+            }
+
+        }
+    }
+}*/
