5c7d689a2b60977b126bbc710554732d2d09ab75
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:41:39 +0530

    Add files via upload

---
+++ Class232.cs
@@ -0,0 +1,40 @@
+﻿//SortedList
+/*using System;
+using System.Collections;
+
+
+namespace ConsoleApp2
+{
+    internal class Class232
+    {
+        static void Main()
+        {
+            SortedList sl=new SortedList();
+            sl.Add("001", "Zara Ali");
+            sl.Add("002", "Abida Rehman");
+            sl.Add("003", "Joe Holzner");
+            sl.Add("004", "Mausam Benazir Nur");
+            sl.Add("005", "M. Amlan");
+            sl.Add("006", "M. Arif");
+            sl.Add("007", "Ritesh Saikia");
+
+            if(sl.Contains("Nahu Ali"))
+            {
+                Console.WriteLine("This is not present in the list");
+
+            }
+            else
+            {
+                sl.Add("008", "Nahu Ali");
+            }
+
+
+            // Get a collection of the keys.
+            ICollection key =sl.Keys;
+            foreach(string k in key)
+            {
+                Console.WriteLine(k+":"+sl[k]);
+            }
+        }
+    }
+}*/
