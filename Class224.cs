e19f97be23b2e9aaafcd90ffc0def7cef78c35bb
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:37:42 +0530

    Add files via upload

---
+++ Class224.cs
@@ -0,0 +1,47 @@
+﻿//Another example of stack
+/*using System;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    internal class Class224
+    {
+        static void Main()
+        {
+            Stack st = new Stack();
+            st.Push("Amar");
+            st.Push("Maria");
+            st.Push("Gomes");
+            st.Push("Wilson");
+
+            Console.WriteLine("Current stack");
+            foreach(string s in st)
+            {
+                Console.WriteLine(s+" ");
+            }
+
+            st.Push("Ketan");
+            st.Push("Vikrant");
+            Console.WriteLine("The next poppable value in stack={0}",st.Peek ());
+
+            Console.WriteLine("Current stack");
+            foreach (string s in st)
+            {
+                Console.WriteLine(s + " ");
+            }
+
+            st.Pop();
+            st.Pop();
+            st.Pop();
+
+            Console.WriteLine("C removing stack");
+            foreach (string s in st)
+            {
+                Console.WriteLine(s + " ");
+            }
+
+
+
+        }
+    }
+}*/
