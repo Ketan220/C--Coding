66ea27ef7fe5e50142b7e7135e9a7fff7927c68d
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-03 21:39:56 +0530

    Add files via upload

--- Class228.cs
+++ Class228.cs
@@ -1,8 +1,7 @@
-﻿/*using System;
+﻿//LinkedList class od generic collection
+/*using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp2
 {
@@ -10,16 +9,46 @@ namespace ConsoleApp2
     {
         static void Main()
         {
+            LinkedList<int> nums = new LinkedList<int>();
 
-            int i, j;
-            
-            for (i = 1; i <= 5; i++)
+            nums.AddLast(23);
+            nums.AddLast(34);
+            nums.AddLast(33);
+            nums.AddLast(11);
+            nums.AddLast(6);
+
+
+            foreach (int num in nums)
             {
-                for (j = 1; j <= i; j++)
-                {
-                    Console.Write(j);
-                }
-                Console.Write("\n");
+                Console.WriteLine("Num="+num);
+            }
+
+
+            Console.WriteLine("After adding in the element in first");
+            nums.AddFirst(9);
+            nums.AddFirst(7);
+            foreach (int num in nums)
+            {
+                Console.WriteLine("Num=" + num);
+            }
+
+
+            Console.WriteLine("To add 5 before 6 in the list");
+            LinkedListNode<int> node = nums.Find(6);
+            nums.AddBefore(node, 5);
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine("Num="+num);
+            }
+
+            Console.WriteLine("To add 5 After 6 in the list");
+            LinkedListNode<int> node1 = nums.Find(6);
+            nums.AddAfter(node1, 5);
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine("Num="+num);
             }
         }
     }
