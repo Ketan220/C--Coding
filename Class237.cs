373658dcfefd313db562ad9b89879f3e300d535b
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-04 19:21:21 +0530

    Add files via upload

---
+++ Class237.cs
@@ -0,0 +1,35 @@
+﻿//Another example
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    delegate int dell(int n1, int n2);
+    class program
+    {
+        public int addition(int n1,int n2)
+        {
+            return n1 + n2;
+        }
+        public int substraction(int n1,int n2)
+        {
+            return n1 - n2;
+        }
+        static void Main()
+        {
+            program p=new program();
+            dell d1 = new dell(p.addition);
+            int res=d1(2, 3);
+            Console.WriteLine("Addition=" + res);
+                
+            dell d2 = new dell(p.substraction);
+             res=d2(2, 3);
+            Console.WriteLine("Substraction="+res);
+
+        }
+
+    }
+}*/
