38d2a70e8205a7b8bdb6e329afe69fae3a0e0dc4
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-04 19:21:03 +0530

    Add files via upload

---
+++ Class236.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    //declare a delegate to hold ref to a method with one int parameter and which return type is int
+    delegate int del_square(int n1);
+    class pragram
+    {
+        //creating a method square with one int parameter and return type is int
+        public int square(int n1)
+        {
+            return n1 * n1;
+        }
+        static void Main()
+        {
+            pragram p=new pragram();
+            del_square d1 = new del_square(p.square);
+            int res=d1(3);
+            Console.WriteLine("Square="+res);
+        }
+    }
+}*/
