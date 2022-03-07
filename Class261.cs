4079e62678da22f9dfe4b7907bf24cd0964585aa
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-07 20:44:54 +0530

    Add files via upload

---
+++ Class261.cs
@@ -0,0 +1,30 @@
+﻿//Example of outofmemoryexception class
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.OOPS_Problems
+{
+    internal class Class261
+    {
+        static void Main()
+        {
+            string name = "booby";
+            string subject = "mathmatics";
+            Console.WriteLine ("Name length="+name.Length);
+            StringBuilder sb=new StringBuilder(name.Length ,name.Length);
+
+            try
+            {
+                sb.Append(subject);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}*/
