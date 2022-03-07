1c8bd6a9ee9c55d30ed0095321b1e2a7429db2f0
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-07 20:44:05 +0530

    Add files via upload

---
+++ Class259.cs
@@ -0,0 +1,21 @@
+﻿//Stringbuilder class is used to append string to an existing string
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.OOPS_Problems
+{
+    internal class Class259
+    {
+        static void Main()
+        {
+            StringBuilder sb= new StringBuilder();
+            sb.Append("Ketan");
+            sb.Append("Bhange");
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine("Bye");
+        }
+    }
+}*/
