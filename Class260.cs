ec5fb259d4a7639feaf2430a1f880ab4bd9c0ab8
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-07 20:44:36 +0530

    Add files via upload

---
+++ Class260.cs
@@ -0,0 +1,33 @@
+﻿//Example of invalidcastexception class
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.OOPS_Problems
+{
+    internal class Class260
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Krunal");
+            sb.Append("Meshram");
+            Console.WriteLine("sb="+sb.ToString());
+
+            object obj = sb;
+            Console.WriteLine(obj.ToString());
+
+            try
+            {
+                System.IO.StreamReader sr = (System.IO.StreamReader)obj;
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}*/
