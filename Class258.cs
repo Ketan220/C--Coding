d704026c044acb92fcddd97893650de9e7bf826c
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-07 20:43:49 +0530

    Add files via upload

---
+++ Class258.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.OOPS_Problems
+{
+    internal class Class258
+    {
+        static void Main()
+        {
+            string str = null;
+
+            try
+            {
+                Console.WriteLine(str.Substring(5));
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}*/
