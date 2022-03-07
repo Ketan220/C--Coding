a985f716618fb1a0acd50b45898edcfc9f4cce2f
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-07 20:42:47 +0530

    Add files via upload

---
+++ Class254.cs
@@ -0,0 +1,39 @@
+﻿//•	finally: The finally block is used to execute a given set of statements, whether an exception is thrown or not thrown.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.OOPS_Problems
+{
+    internal class Class254
+    {
+        static void Main()
+        {
+            int num;
+            Console.WriteLine("Enter number 1");
+            num=Convert.ToInt32(Console.ReadLine());
+            int n;
+            Console.WriteLine("Enter number 2");
+            n=Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+
+            try
+            {
+                res = num / n; //error will come here and program terminates
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("catch block execute");
+                Console.WriteLine("Cannot divided by zero");
+            }
+            finally
+            {
+                Console.WriteLine("Final block execute");
+                Console.WriteLine("Result="+res);
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}*/
