6b3c449d3eb076a73a2e61d35b00383db2b86db6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-07 20:42:16 +0530

    Add files via upload

---
+++ Class252.cs
@@ -0,0 +1,20 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class252
+    {
+        static void Main()
+        {
+            int num = 10;
+            int n = 0;
+            int res = num / n; //error will come here and program terminates
+            Console.WriteLine("Result="+res);
+            Console.WriteLine("Bye");
+        }
+    }
+}*/
