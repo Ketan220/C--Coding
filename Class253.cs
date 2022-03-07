6f41b2e716704123989e835ae130591b5df5b480
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-07 20:42:30 +0530

    Add files via upload

---
+++ Class253.cs
@@ -0,0 +1,34 @@
+﻿//The catch keyword indicates the catching of an exception.
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.OOPS_Problems
+{
+    internal class Class253
+    {
+        static void Main()
+        {
+            int num;
+            Console.WriteLine("Enter number 1");
+            num = Convert.ToInt32(Console.ReadLine());
+            int n;
+            Console.WriteLine("Enter number  2");
+            n = Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+            
+            try
+            {
+                res = num / n; //error will come here and program terminates
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Number should not be devided by zero");
+            }
+            Console.WriteLine("Result=" + res);
+            Console.WriteLine("Bye");
+        }
+    }
+}*/
