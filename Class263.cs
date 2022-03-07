e5a634ca8ccd80fc3e296630f364eddc78e03673
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-07 20:45:26 +0530

    Add files via upload

---
+++ Class263.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.OOPS_Problems
+{
+    class Temperature
+    {
+        int temperature = 0;
+        public void showtemp()
+        {
+            if (temperature == 0)
+            {
+                throw new TempIsZeroException("Zero temperature found");
+            }
+            else
+            {
+                Console.WriteLine("Temperature:{0}" + temperature);
+            }
+        }
+    }
+
+    public class TempIsZeroException : ApplicationException
+    {
+        public TempIsZeroException(string message) : base(message)
+        {
+
+        }
+
+    }
+
+    internal class Class2631
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            try
+            {
+                temp.showtemp();
+            }
+            catch (TempIsZeroException e)
+            {
+                Console.WriteLine("TempIsZeroException: {0}", e.Message);
+            }
+            Console.WriteLine("Bye");
+
+        }
+    }
+}
