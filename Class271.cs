4bf37c1a3c0f5ace31ed7f50737de2c849c93590
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-08 16:07:19 +0530

    Add files via upload

---
+++ Class271.cs
@@ -0,0 +1,31 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+   
+    internal class program
+    {
+        public void display()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("I=" + i);
+                Thread.Sleep(500);
+            }
+        }
+        static void Main()
+        {
+            program p=new program();
+            ThreadStart ts=new ThreadStart(p.display);
+            Thread t1 = new Thread(ts);
+            Thread t2 = new Thread(ts);
+            t1.Start();
+            t1.Join();
+            t2.Start();
+        }
+    }
+}*/
