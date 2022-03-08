aa1fbd152b71faaa01faac3759302f5ff70e3e0c
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-03-08 15:56:21 +0530

    Add files via upload

---
+++ Class264.cs
@@ -0,0 +1,19 @@
+﻿//The following program demonstrates main thread execution −
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class264
+    {
+        static void Main()
+        {
+            Thread th=Thread .CurrentThread;
+            th.Name = "Main thread";
+            Console.WriteLine("This is {0}"+th.Name);               
+        }
+    }
+}*/
